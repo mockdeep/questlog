31e00ba0686a15168432fb19f3bf505bc5342a86
Robert Fletcher <lobatifricha@gmail.com>
2013-04-12 19:39:57 -0700

    trying to set up guard

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,22 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
+require 'rubygems'
+require 'spork'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require 'rspec/rails'
+  require 'rspec/autorun'
 
-RSpec.configure do |config|
-  config.include(FactoryGirl::Syntax::Methods)
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
-  config.infer_base_class_for_anonymous_controllers = false
-  config.order = "random"
+  RSpec.configure do |config|
+    config.include(FactoryGirl::Syntax::Methods)
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    config.use_transactional_fixtures = true
+    config.infer_base_class_for_anonymous_controllers = false
+    config.order = "random"
+  end
 end
+
+Spork.each_run do
+  require File.expand_path("../../config/environment", __FILE__)
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+end
+
