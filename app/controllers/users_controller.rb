06cc0533bcdaf0efaa368a4293937198496cd9ba
Robert Fletcher <lobatifricha@gmail.com>
2013-05-12 22:42:18 -0700

    add mode to users, plus simplified view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    current_user.update_attributes!(user_params)
+    redirect_to root_path
+  end
+
   private
 
   def user_params
-    params[:user].permit(:email, :password, :password_confirmation)
+    params[:user].permit(:email, :password, :password_confirmation, :mode)
   end
 end
