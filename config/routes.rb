636974faa41c1cdf20597936797d36aaee6ba60c
Robert Fletcher <lobatifricha@gmail.com>
2013-06-29 19:45:38 -0700

    adding what link

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Quickies::Application.routes.draw do
   resources :quickies, only: [:create, :update, :destroy]
   resources :users, only: [:new, :create, :update]
 
+  get '/what', to: 'pages#what', as: 'what'
+
   get '/:slug', to: 'quickies#show', as: 'context'
 end
