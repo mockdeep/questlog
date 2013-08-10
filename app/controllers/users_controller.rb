3395159fd0ab6edcc4c5806c91da83a98d92b10f
Robert Fletcher <lobatifricha@gmail.com>
2013-08-09 20:08:33 -0700

    refactoring, fixing advanced mode switch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,6 @@
 class UsersController < ApplicationController
   skip_before_filter :authenticate_user!
 
-  def new
-    @account = FreeAccount.new
-  end
-
-  def create
-    @account = FreeAccount.new(account_params)
-    if @account.save
-      current_user.update_attributes!(account: @account)
-      flash[:notice] = 'Signed up!'
-      redirect_to root_path
-    else
-      flash.now[:error] = 'There was a problem creating your account...'
-      render :new
-    end
-  end
-
   def update
     current_user.update_attributes!(user_params)
     redirect_to root_path
@@ -24,10 +8,8 @@ class UsersController < ApplicationController
 
   private
 
-  def account_params
-    params[:free_account].permit(:email,
-                                 :password,
-                                 :password_confirmation,
-                                 :mode)
+  def user_params
+    params[:user].permit(:mode)
   end
+
 end
