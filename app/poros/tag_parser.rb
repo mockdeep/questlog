364604f60bcdbea74bfabea5e58f00d480877aa7
Robert Fletcher <lobatifricha@gmail.com>
2014-06-28 13:34:35 -0700

    remove support for quoted tag names

--- app/poros/tag_parser.rb
+++ app/poros/tag_parser.rb
@@ -2,17 +2,15 @@ class TagParser
 
   def parse(title)
     tag_names = []
-    tag_regexes.each do |regex|
-      title.scan(regex) do |matches|
-        tag_names << matches.first
-      end
-      title = title.gsub(regex, '').strip
+    title.scan(tag_regex) do |matches|
+      tag_names << matches.first
     end
+    title = title.gsub(tag_regex, '').strip
     [title, tag_names]
   end
 
-  def tag_regexes
-    [/\#(\w+)/, /\#"(.*?)"/, /\#'(.*?)'/]
+  def tag_regex
+    /\#(\S+)/
   end
 
 end
