92ed045d09cf1213bf71bb305c0fe9bdf21f341f
Robert Fletcher <lobatifricha@gmail.com>
2014-07-06 12:39:33 +0800

    ensure title parser has no keys for missing markers

--- spec/poros/title_parser/parse_title_spec.rb
+++ spec/poros/title_parser/parse_title_spec.rb
@@ -5,15 +5,20 @@ describe TitleParser, '#parse_title' do
   let(:parser) { TitleParser.new }
 
   it 'pulls the markers out of the string' do
-    result = parser.parse_title('#at-home take out trash !3 #lame')
+    result = parser.parse_title('#at-home take out trash !3 #lame *1w ~5mi')
     expect(result[:title]).to eq 'take out trash'
     expect(result[:tag_names].sort).to eq %w(at-home lame)
     expect(result[:priority]).to eq 3
+    expect(result[:repeat]).to eq 1.week
+    expect(result[:estimate]).to eq 5.minutes
   end
 
-  it 'returns an empty array for tag_names when no tags are in the string' do
-    result = parser.parse_title('do something')
-    expect(result[:tag_names]).to eq []
+  it 'returns no keys for missing markers' do
+    result = parser.parse_title('nothing special')
+    expect(result[:title]).to eq 'nothing special'
+    [:tag_names, :priority, :repeat, :estimate].each do |marker|
+      expect(result).not_to have_key(marker)
+    end
   end
 
   it 'returns an array of tag_names when there are #tags in the string' do
