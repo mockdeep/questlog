43dd9d3210e56f33ebdae2024344dc0df8713d5f
Robert Fletcher <lobatifricha@gmail.com>
2014-08-16 16:20:56 -0700

    Adding ' parsing for priority

--- app/poros/priority_parser.rb
+++ app/poros/priority_parser.rb
@@ -7,6 +7,7 @@ class PriorityParser
   def parse(title)
     words = title.split
     index = words.index { |word| word.match(/^!\d$/) }
+    index ||= words.index { |word| word.match(/^'\d$/) }
     match = words.delete_at(index) if index
     priority = Integer(match[1..-1]) if match
     [words.join(' '), priority]
--- spec/poros/priority_parser/parse_spec.rb
+++ spec/poros/priority_parser/parse_spec.rb
@@ -22,4 +22,18 @@ describe PriorityParser, '#parse' do
     expect(parser.parse('not!1')).to eq ['not!1', nil]
   end
 
+  it "returns the numeric priority when there is a '1 priority in the string" do
+    expect(parser.parse("'1 pay the bills")).to eq ['pay the bills', 1]
+    expect(parser.parse("eat '2 beef")).to eq ['eat beef', 2]
+    expect(parser.parse("eat boogers '3")).to eq ['eat boogers', 3]
+    expect(parser.parse("e'3at boogers '3")).to eq ["e'3at boogers", 3]
+    expect(parser.parse("pri '1 not '2")).to eq ["pri not '2", 1]
+  end
+
+  it "returns nil for priority when there is an 'improper priority" do
+    expect(parser.parse("'what?")).to eq ["'what?", nil]
+    expect(parser.parse("'1way")).to eq ["'1way", nil]
+    expect(parser.parse("not'1")).to eq ["not'1", nil]
+  end
+
 end
