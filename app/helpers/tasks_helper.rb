5d0f61611cd0b4d14cee9690709471dabbdb86a8
Robert Fletcher <lobatifricha@gmail.com>
2014-08-18 10:16:13 -0700

    increase skip count

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -3,7 +3,7 @@ module TasksHelper
   def task_classes(task)
     classes = []
     classes << "priority-#{task.priority}" if task.priority?
-    classes << 'over_skipped' if task.skip_count >= 5
+    classes << 'over_skipped' if task.skip_count >= 15
     classes.join(' ')
   end
 
