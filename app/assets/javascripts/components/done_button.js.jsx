84058ba9fe16f4527ba696d5dc931e67435880d9
Robert Fletcher <lobatifricha@gmail.com>
2014-10-06 08:23:25 -0700

    move duplicate logic to helper

--- app/assets/javascripts/components/done_button.js.jsx
+++ app/assets/javascripts/components/done_button.js.jsx
@@ -4,26 +4,10 @@
   'use strict';
 
   Questlog.DoneButton = React.createClass({
-
-    reloadPage: function () {
-      window.location.reload();
-    },
-
-    logError: function (error) {
-      console.log(error);
-    },
-
     markDone: function () {
-      reqwest({
+      Questlog.request({
         url: 'tasks/' + this.props.id,
         data: { task: { done: true } },
-        type: 'json',
-        method: 'put',
-        headers: {
-          'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-        },
-        success: this.reloadPage,
-        error: this.logError
       });
     },
 
@@ -32,7 +16,6 @@
         <input type='button' className='btn btn-primary btn-large btn-block' onClick={this.markDone} value='Done! Give me another!' />
       );
     }
-
   });
 
 })();
