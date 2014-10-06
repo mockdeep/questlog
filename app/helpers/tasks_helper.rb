27fbcbd01e72242c57709607ec6bce5c911271e2
Robert Fletcher <lobatifricha@gmail.com>
2014-10-06 07:59:53 -0700

    remove no longer used postpone options

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,30 +1,10 @@
 module TasksHelper
 
-  POSTPONE_OPTIONS = {
-    '5 minutes' => 5.minutes,
-    '30 minutes' => 30.minutes,
-    '1 hour' => 1.hour,
-    '3 hours' => 3.hours,
-    '6 hours' => 6.hours,
-    '9 hours' => 9.hours,
-    '12 hours' => 12.hours,
-    '1 day' => 1.day,
-    '2 days' => 2.days,
-    '3 days' => 3.days,
-    '1 week' => 1.week,
-    '2 weeks' => 2.weeks,
-    '1 month' => 1.month,
-  }
-
   def task_classes(task)
     classes = []
     classes << "priority-#{task.priority}" if task.priority?
     classes << 'over_skipped' if task.skip_count >= 15
     classes.join(' ')
-  end
-
-  def postpone_options
-    POSTPONE_OPTIONS
   end
 
 end
