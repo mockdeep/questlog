af7f84b045f0fe175f126e3e8e3699529ddb463f
Robert Fletcher <lobatifricha@gmail.com>
2014-10-16 08:21:32 -0700

    set new task form up as react component

---
+++ app/assets/javascripts/components/new_task_form.js.jsx
@@ -0,0 +1,32 @@
+/** @jsx React.DOM */
+
+(function () {
+  'use strict';
+
+  Questlog.NewTaskForm = React.createClass({
+    saveTask: function (event) {
+      event.preventDefault();
+      var taskTitle = this.refs.title.getDOMNode().value.trim();
+      Questlog.request({
+        url: 'tasks',
+        method: 'post',
+        data: { task: { title: taskTitle } },
+        success: Questlog.reloadPage()
+      });
+    },
+    render: function () {
+      return (
+        <form className='new_task' id='new_task' onSubmit={this.saveTask}>
+          <div className='row-fluid'>
+            <div className='span6'>
+              <input autocomplete='off' className='span12' id='new_title' type='text' ref='title' />
+            </div>
+            <div className='span6'>
+              <input className='btn btn-success btn-block' type='submit' value='Add Task' />
+            </div>
+          </div>
+        </form>
+      );
+    }
+  });
+})();
