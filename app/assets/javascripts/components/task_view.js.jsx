6b58a0b9f4ab83ad4e1512be447374f344b5f4a2
Robert Fletcher <lobatifricha@gmail.com>
2014-10-29 07:48:49 -0700

    move more of task view to component

---
+++ app/assets/javascripts/components/task_view.js.jsx
@@ -0,0 +1,46 @@
+/** @jsx React.DOM */
+
+(function () {
+
+  'use strict';
+
+  Questlog.TaskView = React.createClass({
+    className: function () {
+      var classString = 'row-fluid';
+      if (this.props.task.priority) {
+        classString = classString + ' priority-' + this.props.task.priority;
+      }
+      if (this.props.task.skip_count >= 15) {
+        classString = classString + ' over_skipped';
+      }
+      return classString;
+    },
+    title: function () {
+      return "skip count: " + this.props.task.skip_count;
+    },
+    repeatIcon: function () {
+      if (this.props.task.repeat_seconds) {
+        return <i className='fa fa-repeat' />;
+      } else {
+        return '';
+      }
+    },
+    render: function () {
+      return (
+        <div id='task' className={this.className()}>
+          <div className='span1'>
+            <Questlog.DeleteButton task={this.props.task} />
+          </div>
+          <div className='span10 title' title={this.title()}>
+            {this.props.task.title}
+            {this.repeatIcon()}
+          </div>
+          <div className='span1'>
+            <i className='fa fa-arrow-down' id='edit-task' title='edit task' />
+          </div>
+        </div>
+      );
+    }
+  });
+
+})();
