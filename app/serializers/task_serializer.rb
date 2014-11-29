30add00851ab22f7a25a42bef241d79e5c4125ae
Robert Fletcher <lobatifricha@gmail.com>
2014-11-28 17:00:25 -0800

    integrate tags with TaskDisplay

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,5 +1,5 @@
 class TaskSerializer < ActiveModel::Serializer
 
-  attributes :id, :priority, :title, :repeat_seconds, :skip_count
+  attributes :id, :priority, :title, :repeat_seconds, :skip_count, :tag_names
 
 end
