ff6beec710d2d1f010aaae22a8500d6bae4492e4
Robert Fletcher <lobatifricha@gmail.com>
2014-12-04 07:25:49 -0800

    update gems, rubocop fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,8 @@ preload_app true
 
 before_fork do |_server, _worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Rails.logger.info 'Unicorn master intercepting TERM and sending myself ' \
+      'QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
 
@@ -13,8 +14,8 @@ end
 
 after_fork do |_server, _worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for ' \
-      'master to send QUIT'
+    Rails.logger.info 'Unicorn worker intercepting TERM and doing nothing. ' \
+      'Wait for master to send QUIT'
   end
 
   if defined?(ActiveRecord::Base)
