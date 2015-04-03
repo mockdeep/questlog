770b733d56d9e90f19ff68388f78db2129adfb83
Robert Fletcher <lobatifricha@gmail.com>
2015-04-02 18:46:52 -0700

    add a couple of views for future development

---
+++ app/assets/javascripts/components/scratch/scratch_page.js.jsx
@@ -0,0 +1,15 @@
+(function () {
+
+  'use strict';
+
+  Questlog.ScratchPage = React.createClass({
+
+    render: function () {
+      return (
+        <h1>You're on scratch!!!</h1>
+      );
+    }
+
+  });
+
+})();
