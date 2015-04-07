809c06cfd4f6795994b36cb8f0ec59ef95e7c483
Robert Fletcher <lobatifricha@gmail.com>
2015-04-07 08:08:46 -0700

    fix migration

--- db/migrate/20150407145353_null_false_positions.rb
+++ db/migrate/20150407145353_null_false_positions.rb
@@ -3,7 +3,6 @@ class NullFalsePositions < ActiveRecord::Migration
   def change
     TaskPositioner.new.position_tasks!
     change_column_null :tasks, :position, false
-    add_index :tasks, :position
     add_index :tasks, [:priority, :position]
   end
 
