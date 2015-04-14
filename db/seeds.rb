7faf09f97bfb7979dc9642fe0990fc82d53c16eb
Robert Fletcher <lobatifricha@gmail.com>
2015-04-14 09:19:03 -0700

    seed some data

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+account = FreeAccount.create(
+  email: 'demo@questlog.io',
+  password: 'pizzas',
+  password_confirmation: 'pizzas',
+)
+
+user = User.create(account: account)
+
+home_tag = user.tags.create(name: 'home')
+work_tag = user.tags.create(name: 'work')
+errand_tag = user.tags.create(name: 'errand')
+
+user.tasks.create(title: 'feed dog', tags: [home_tag])
+user.tasks.create(title: 'do work', tags: [work_tag])
+user.tasks.create(title: 'buy groceries', tags: [errand_tag])
+user.tasks.create(title: 'read feeds')
