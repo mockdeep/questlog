e2767932fa6b2d3681611747f2ef16e3ff30041a
Robert Fletcher <lobatifricha@gmail.com>
2015-04-20 08:25:32 -0700

    move the last component to require

--- app/assets/javascripts/components/timeframes_index.js.jsx
+++ app/assets/javascripts/components/timeframes_index.js.jsx
@@ -1,10 +1,11 @@
 (function () {
-
   'use strict';
 
-  Questlog.TimeframesIndex = React.createClass({
+  var TimeframesIndex = React.createClass({
     render: function () {
       return (<h1>Welcome to timeframes</h1>);
     }
   });
+
+  module.exports = TimeframesIndex;
 })();
