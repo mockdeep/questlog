9fd9c5a075ad78425cf47be21be8020945b4ea54
Robert Fletcher <lobatifricha@gmail.com>
2015-04-22 08:50:35 -0700

    make ordered spec consistent

--- spec/models/task/class_methods/ordered_spec.rb
+++ spec/models/task/class_methods/ordered_spec.rb
@@ -1,11 +1,13 @@
 RSpec.describe Task, '.ordered' do
 
+  let(:user) { create(:user) }
+
   it 'returns tasks with priority first, position second' do
-    task_1 = create(:task)
-    task_2 = create(:task, priority: 3)
-    task_3 = create(:task, priority: 1)
-    task_4 = create(:task, priority: 2)
-    task_5 = create(:task)
+    task_1 = create(:task, user: user)
+    task_2 = create(:task, user: user, priority: 3)
+    task_3 = create(:task, user: user, priority: 1)
+    task_4 = create(:task, user: user, priority: 2)
+    task_5 = create(:task, user: user)
 
     expect(Task.ordered).to eq [task_3, task_4, task_2, task_1, task_5]
   end
