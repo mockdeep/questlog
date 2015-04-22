a19ee7839394f3fc1b6a8274e6aec3e6cce33a74
Robert Fletcher <lobatifricha@gmail.com>
2015-04-22 09:48:52 -0700

    remove a couple more IIFEs

--- app/assets/javascripts/helpers.js
+++ app/assets/javascripts/helpers.js
@@ -1,52 +1,50 @@
-(function () {
-  'use strict';
+'use strict';
 
-  var reqwest = require('reqwest');
+var reqwest = require('reqwest');
 
-  var reloadPage = function () {
-    window.location.reload();
+var reloadPage = function () {
+  window.location.reload();
+};
+
+var logError = function (error) {
+  console.log('error: ', error.statusText);
+};
+
+var authenticityToken = function () {
+  return $('meta[name="csrf-token"]').attr('content');
+};
+
+var stopPropagation = function (event) {
+  event.stopPropagation();
+};
+
+var request = function (options) {
+  reqwest(mergeOptions(defaultRequestOptions(), options));
+};
+
+var flash = function (status, message) {
+  var $myFlash = $('<div />', { class: 'flash-' + status, text: message });
+  $('#flashes').append($myFlash);
+  $('[class^=flash-]').fadeOut(1500);
+};
+
+function defaultRequestOptions() {
+  return {
+    type: 'json',
+    method: 'put',
+    headers: { 'X-CSRF-Token': authenticityToken() },
+    success: reloadPage,
+    error: logError
   };
+}
 
-  var logError = function (error) {
-    console.log('error: ', error.statusText);
-  };
+function mergeOptions(defaults, options) {
+  return $.extend({}, defaults, options);
+}
 
-  var authenticityToken = function () {
-    return $('meta[name="csrf-token"]').attr('content');
-  };
-
-  var stopPropagation = function (event) {
-    event.stopPropagation();
-  };
-
-  var request = function (options) {
-    reqwest(mergeOptions(defaultRequestOptions(), options));
-  };
-
-  var flash = function (status, message) {
-    var $myFlash = $('<div />', { class: 'flash-' + status, text: message });
-    $('#flashes').append($myFlash);
-    $('[class^=flash-]').fadeOut(1500);
-  };
-
-  function defaultRequestOptions() {
-    return {
-      type: 'json',
-      method: 'put',
-      headers: { 'X-CSRF-Token': authenticityToken() },
-      success: reloadPage,
-      error: logError
-    };
-  }
-
-  function mergeOptions(defaults, options) {
-    return $.extend({}, defaults, options);
-  }
-
-  module.exports = {
-    authenticityToken: authenticityToken,
-    flash: flash,
-    request: request,
-    stopPropagation: stopPropagation
-  };
-})();
+module.exports = {
+  authenticityToken: authenticityToken,
+  flash: flash,
+  request: request,
+  stopPropagation: stopPropagation
+};
