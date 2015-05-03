bffc873217ccebc5cace4106adab7de563c136a4
Robert Fletcher <lobatifricha@gmail.com>
2015-05-03 16:01:55 -0700

    switch to using npm jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,12 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY
 // BLANK LINE SHOULD GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery2
+//= require_self
 //= require jquery_ujs
 //= require bootstrap
-//= require_self
 //= require tasks
+
+$ = jQuery = require('jquery');
 
 require('es5-shim');
 
