ddd5bec2793bb7c122e53e33b61d3dca661a5c69
Robert Fletcher <lobatifricha@gmail.com>
2015-05-07 07:46:39 -0700

    detach StatCreate from specs

--- spec/actions/stat_create/call_spec.rb
+++ spec/actions/stat_create/call_spec.rb
@@ -1,14 +1,14 @@
 RSpec.describe StatCreate, '#call' do
 
-  let(:mock_stat) { class_spy(Stat) }
-  let(:stat_create) { StatCreate.new(stat_class: mock_stat) }
+  let(:mock_stat_class) { class_spy(Stat) }
+  let(:stat_create) { StatCreate.new(stat_class: mock_stat_class) }
   let(:date) { 1.month.ago }
 
   it 'creates a Stat' do
     stat_create.(user: 'foo', value: 30, timestamp: date)
 
     expected_args = { user: 'foo', value: 30, timestamp: date.to_date }
-    expect(mock_stat).to have_received(:create!).with(expected_args)
+    expect(mock_stat_class).to have_received(:create!).with(expected_args)
   end
 
 end
--- spec/controllers/timeframes_controller/index_spec.rb
+++ spec/controllers/timeframes_controller/index_spec.rb
@@ -4,16 +4,12 @@ RSpec.describe TimeframesController, '#index' do
 
   before(:each) { login_as(user) }
 
-  def create_stat(params)
-    StatCreate.new(stat_class: Stat).(params)
-  end
-
   it 'returns the median productivity for the current user' do
-    create_stat(user: user, value: 35.minutes, timestamp: 1.month.ago)
-    create_stat(user: user, value: 1.hour, timestamp: 1.week.ago)
+    Stat.create!(user: user, value: 35.minutes, timestamp: 1.month.ago)
+    Stat.create!(user: user, value: 1.hour, timestamp: 1.week.ago)
     get(:index)
     expect(JSON.parse(response.body)).to eq('medianProductivity' => 1.hour.to_i)
-    create_stat(user: user, value: 35.minutes, timestamp: 5.days.ago)
+    Stat.create!(user: user, value: 35.minutes, timestamp: 5.days.ago)
     get(:index)
     expect(JSON.parse(response.body)).to eq('medianProductivity' => 2850)
   end
