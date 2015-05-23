8adaf150e36125d97a98f47d6b491fa10cff423f
Robert Fletcher <lobatifricha@gmail.com>
2015-05-23 13:55:41 -0700

    add time balancer

---
+++ app/assets/javascripts/time_balancer.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var moment = require('moment');
+
+function daysBetween(startTime, endTime) {
+  return endTime.diff(startTime, 'days');
+}
+
+function timeframes(time) {
+  var endOfWeek = moment(time).endOf('week');
+  var endOfMonth = moment(time).endOf('month');
+  if (endOfMonth.isBefore(endOfWeek)) { endOfMonth = endOfWeek; }
+  var endOfQuarter = moment(time).endOf('quarter');
+  if (endOfQuarter.isBefore(endOfWeek)) { endOfQuarter = endOfWeek; }
+  var endOfYear = moment(time).endOf('year');
+  if (endOfYear.isBefore(endOfWeek)) { endOfYear = endOfWeek; }
+
+  return {
+    today: 1,
+    week: daysBetween(time, endOfWeek),
+    month: daysBetween(endOfWeek, endOfMonth),
+    quarter: daysBetween(endOfMonth, endOfQuarter),
+    year: daysBetween(endOfQuarter, endOfYear)
+  };
+}
+
+var TimeBalancer = {
+  base_balances: function (time) {
+    time = time || moment();
+    return timeframes(time);
+  }
+}
+
+module.exports = TimeBalancer;
