d79511627c1dbe73fe4af6f58c086615dbfa802c
Robert Fletcher <lobatifricha@gmail.com>
2015-06-06 17:04:30 -0700

    create and employ bulk task store

---
+++ app/assets/javascripts/stores/bulk_task_store.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var request = require('../helpers').request;
+
+module.exports = {
+
+  url: '/bulk_tasks',
+
+  create: function (attrs) {
+    return request({
+      url: this.url,
+      method: 'post',
+      data: {bulk_task: attrs},
+      success: function () { /* do nothing */ }
+    });
+  }
+};
+
