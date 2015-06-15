95273d96c88809c7c8f33429c6968e4e1fd704f2
Robert Fletcher <lobatifricha@gmail.com>
2015-06-15 09:29:31 -0700

    EditTaskForm with no promise

--- app/assets/javascripts/components/tasks/_task_display.jsx
+++ app/assets/javascripts/components/tasks/_task_display.jsx
@@ -23,10 +23,7 @@ var TaskDisplay = React.createClass({
           disable={this.props.disable}
         />
         <hr />
-        <EditTaskForm
-          task={this.props.task}
-          loadTask={this.props.loadTask}
-        />
+        <EditTaskForm task={this.props.task} />
       </div>
     );
   }
