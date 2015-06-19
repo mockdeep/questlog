c74c4edf6fb5e13b5c6b804185c6b5a41d08939e
Robert Fletcher <lobatifricha@gmail.com>
2015-06-19 07:59:43 -0700

    fix drag/drop re-ordering on index

--- app/assets/javascripts/stores/bulk_task_store.js
+++ app/assets/javascripts/stores/bulk_task_store.js
@@ -2,10 +2,24 @@
 
 var _ = require('lodash');
 
+var request = require('../helpers').request;
+
 var RestfulStore = require('./restful_store');
 var TaskStore = require('./task_store');
 
-var BulkTaskStore = _.extend({}, RestfulStore, { name: 'bulk_task' });
+var BulkTaskStore = _.extend({}, RestfulStore, {
+  name: 'bulk_task',
+
+  update: function (attrs) {
+    var data = {};
+    data[this.name] = attrs;
+    return request({
+      url: this.url(),
+      data: data,
+      success: this.unload.bind(this)
+    });
+  }
+});
 
 BulkTaskStore.on('change', function () { TaskStore.unload(); });
 module.exports = BulkTaskStore;
