f6d52ec16a8f791acbe4c51469a939d05a769c26
Robert Fletcher <lobatifricha@gmail.com>
2015-06-21 16:51:57 -0700

    setting up TimeframeStore to grab from TaskStore

--- app/assets/javascripts/stores/timeframe_store.js
+++ app/assets/javascripts/stores/timeframe_store.js
@@ -6,15 +6,58 @@ var request = require('../helpers').request;
 var RestfulStore = require('./restful_store');
 var TaskStore = require('./task_store');
 
+var timeframeList = [
+  'inbox',
+  'today',
+  'week',
+  'month',
+  'quarter',
+  'year',
+  'lustrum',
+  'decade'
+]
+
+var medianProductivity;
+
 var TimeframeStore = _.extend({}, RestfulStore, {
   name: 'timeframe',
 
-  getAll: function () {
-    return request({
-      method: 'get',
-      url: this.url(),
-      success: function () { /* do nothing */ }
+  updateModels: function (data) {
+    var tasks = data.tasks;
+    var timeframes = {};
+    timeframeList.forEach(function (timeframeName) {
+      timeframes[timeframeName] = { name: timeframeName, tasks: [] };
     });
+    tasks.forEach(function (task) {
+      var timeframeName = task.timeframe || 'inbox';
+      timeframes[timeframeName].tasks.push(task);
+    });
+    this.models = timeframeList.map(function (timeframeName) {
+      return timeframes[timeframeName];
+    });
+  },
+
+  getData: function () {
+    return {
+      timeframes: this.models,
+      meta: {medianProductivity: medianProductivity}
+    };
+  },
+
+  getAll: function () {
+    return new Promise(function (resolve, reject) {
+      TaskStore.getAll().then(function (data) {
+        this.updateModels(data);
+        request({
+          method: 'get',
+          url: this.url(),
+          success: function (data) {
+            medianProductivity = data.meta.medianProductivity;
+            resolve(this.getData());
+          }.bind(this)
+        });
+      }.bind(this));
+    }.bind(this));
   }
 });
 
