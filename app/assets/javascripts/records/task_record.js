e4754681ee17f939740f537d602c7e482895a176
Robert Fletcher <lobatifricha@gmail.com>
2015-07-04 09:55:37 -0700

    introduce immutable.js

---
+++ app/assets/javascripts/records/task_record.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var Record = require('immutable').Record;
+
+module.exports = Record({
+  id: null,
+  estimate_seconds: null,
+  pending: null,
+  priority: null,
+  repeat_seconds: null,
+  release_at: null,
+  timeframe: null,
+  title: null,
+  tag_names: null,
+  skip_count: null
+});
--- app/assets/javascripts/stores/task_store.js
+++ app/assets/javascripts/stores/task_store.js
@@ -3,13 +3,15 @@
 var _ = require('lodash');
 
 var RestfulStore = require('./restful_store');
+var TaskRecord = require('../records/task_record');
 
 module.exports = _.extend({}, RestfulStore, {
   name: 'task',
 
   updateModels: function (data) {
-    _.each(data.tasks, function (task) {
+    data.tasks = data.tasks.map(function (task) {
       task.estimate_seconds = task.estimate_seconds || 1800;
+      return new TaskRecord(task);
     });
     RestfulStore.updateModels.call(this, data);
   }
