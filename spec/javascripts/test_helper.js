3e52744ed8d549f026cf64ecc32b7ea6c1cee9ee
Robert Fletcher <lobatifricha@gmail.com>
2015-07-15 08:28:57 -0700

    switch last of specs over to mocha

--- spec/javascripts/test_helper.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var path = require('path');
-var jsPath = path.normalize(__dirname + '../../../app/assets/javascripts/');
-
-componentPath = function (componentName) { return jsPath + componentName; }
--- test/helper.js
+++ test/helper.js
@@ -3,6 +3,12 @@
 var path = require('path');
 var jsPath = path.normalize(__dirname + '../../app/assets/javascripts/');
 
+var jsdom = require('jsdom');
+
+global.document = jsdom.jsdom('<html><body></body></html>');
+global.window = document.parentWindow;
+global.navigator = { userAgent: 'mocha' };
+
 module.exports = {
   componentPath: function (componentName) { return jsPath + componentName; }
 }
