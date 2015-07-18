c88e766f9a1ab0f1848e912f00e479bf29e2796a
Robert Fletcher <lobatifricha@gmail.com>
2015-07-18 16:06:11 -0700

    refactor stat creation

--- app/actions/stat_create.rb
+++ app/actions/stat_create.rb
@@ -4,16 +4,18 @@ class StatCreate
     @stat_value_class = stat_value_class
   end
 
-  def call(value:, user:)
-    find_params = {
-      name: 'seconds-completed',
-      user: user,
-      timestamp: Time.zone.now.beginning_of_day,
-    }
+  def call(value:, user:, name:)
+    find_params = { name: name, user: user, timestamp: today }
     stat = Stat.find_or_initialize_by(find_params)
     stat.value ||= 0
     stat.value += @stat_value_class.new(value: value, user: user)
     stat.save!
+  end
+
+private
+
+  def today
+    Time.zone.now.beginning_of_day
   end
 
 end
