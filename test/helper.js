ce231417ad8dc6852128d4b8834ce0f5341656b7
Robert Fletcher <lobatifricha@gmail.com>
2015-08-08 12:43:03 -0700

    update jsdom, use iojs

--- test/helper.js
+++ test/helper.js
@@ -6,7 +6,7 @@ var jsPath = path.normalize(__dirname + '../../app/assets/javascripts/');
 var jsdom = require('jsdom');
 
 global.document = jsdom.jsdom('<html><body></body></html>');
-global.window = document.parentWindow;
+global.window = document.defaultView;
 global.navigator = { userAgent: 'mocha' };
 
 global.expect = require('chai').expect;
