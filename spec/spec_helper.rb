9d43eaaaee89c084833cea9c1f926ef64324232a
Robert Fletcher <lobatifricha@gmail.com>
2015-12-13 12:07:35 -0800

    disable ruby warnings after all

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,7 +22,6 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.raise_errors_for_deprecations!
-  config.warnings = true
   config.raise_on_warning = true
   config.infer_base_class_for_anonymous_controllers = false
   config.order = 'random'
