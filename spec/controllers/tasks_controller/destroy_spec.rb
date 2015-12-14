36be97da6642325e6e52f357da85df3fa669892c
Robert Fletcher <lobatifricha@gmail.com>
2015-12-13 16:57:51 -0800

    Configure Honeybadger for client side

--- spec/controllers/tasks_controller/destroy_spec.rb
+++ spec/controllers/tasks_controller/destroy_spec.rb
@@ -20,23 +20,6 @@ RSpec.describe TasksController, '#destroy' do
     expect(user.reload.unfinished_tasks_count).to eq 0
   end
 
-  context 'when clicked multiple times' do
-    it 'does not decrement the counter multiple times' do
-      configure_for_threading!
-
-      task = create(:task)
-      user = task.user
-      login_as(user)
-
-      threaded do
-        @controller = TasksController.new
-        delete(:destroy, id: task.id)
-      end
-
-      expect(user.reload.unfinished_tasks_count).to eq 0
-    end
-  end
-
   it 'allows deletion of pending tasks' do
     task.update!(done: true, release_at: 1.hour.from_now)
     expect { delete(:destroy, id: task.id) }.not_to raise_error
