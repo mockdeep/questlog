911306b29ac03ec1007ccbc74571b00259249c3e
Robert Fletcher <lobatifricha@gmail.com>
2015-12-31 13:50:35 -0800

    mockless Median

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -8,12 +8,12 @@ class Stat < ActiveRecord::Base
   validates :timestamp, uniqueness: { scope: :user_id }
   validates :name, presence: true, inclusion: { in: NAMES }
 
-  def self.median_productivity(median_class: Median)
+  def self.median_productivity
     beginning = 2.weeks.ago.beginning_of_day
     ending = Time.zone.now.beginning_of_day
     query = where('timestamp > ? AND timestamp < ?', beginning, ending)
     values = query.pluck(:value)
-    median_class.new(values, default: 1.hour)
+    Median.new(values, default: 1.hour)
   end
 
 end
