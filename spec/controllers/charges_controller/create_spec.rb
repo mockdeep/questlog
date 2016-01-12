44f95045539fed67f70759f9c175690c9453fcfe
Robert Fletcher <lobatifricha@gmail.com>
2016-01-11 20:10:59 -0800

    Specs for `ChargesController`

---
+++ spec/controllers/charges_controller/create_spec.rb
@@ -0,0 +1,51 @@
+RSpec.describe ChargesController, '#create' do
+
+  let(:user) { create(:free_user) }
+  let(:valid_params) do
+    { stripeEmail: 'test@questlog.io', stripeToken: 'a_token' }
+  end
+  let(:mock_customer) { double(id: 'an_id') }
+
+  before(:each) do
+    login_as(user)
+    allow(Stripe::Customer).to receive(:create).and_return(mock_customer)
+    allow(Stripe::Charge).to receive(:create)
+  end
+
+  it 'sets the customer id on the user' do
+    expect do
+      post(:create, valid_params)
+    end.to change { user.reload.customer_id }.from(nil).to('an_id')
+  end
+
+  context 'when charge is successfully created' do
+    it 'renders the a success page' do
+      post(:create, valid_params)
+      expect(response.body).to match(/Thanks, you paid/)
+    end
+  end
+
+  context 'when charge is not successfully created' do
+    before(:each) do
+      error = Stripe::CardError.new('bad news', 'param', 'code')
+      allow(Stripe::Charge).to receive(:create).and_raise(error)
+    end
+
+    it 'flashes an error' do
+      post(:create, valid_params)
+      expect(flash[:error]).to eq 'bad news'
+    end
+
+    it 'redirects to the charges page' do
+      post(:create, valid_params)
+      expect(response).to redirect_to charges_path
+    end
+
+    it 'still sets the customer id on the user' do
+      expect do
+        post(:create, valid_params)
+      end.to change { user.reload.customer_id }.from(nil).to('an_id')
+    end
+  end
+
+end
