9e3949fc14c8a227d9e18313198d1487a315a1b9
Robert Fletcher <lobatifricha@gmail.com>
2016-09-12 18:54:43 -0700

    move help link

--- app/assets/javascripts/components/common/task_footer.jsx
+++ app/assets/javascripts/components/common/task_footer.jsx
@@ -3,7 +3,7 @@
 const React = require('react');
 const Link = require('react-router').Link;
 
-const HelpLink = require('components/common/help_link');
+const HelpLink = require('_common/components/help_link');
 
 const TaskFooter = React.createClass({
   render() {
