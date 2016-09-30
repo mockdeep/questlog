fa6d55e7a9275a3ae1bdbb019361dedc5295cede
Robert Fletcher <lobatifricha@gmail.com>
2016-09-29 19:31:03 -0700

    rename Stone.List to Stone.StoneArray

---
+++ test/_common/stone/array_test.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const StoneArray = require('_common/stone').StoneArray;
+
+const frozenError = /object is not extensible/;
+
+describe('StoneArray', function () {
+  describe('constructor', function () {
+    it('throws an error given no arguments', function () {
+      const message = /must be initialized with an array/;
+
+      expect(function () { return new StoneArray(); }).to.throw(message);
+    });
+
+    it('throws an error when first argument is not an array', function () {
+      const message = /must be initialized with an array/;
+
+      expect(function () { return new StoneArray('foo'); }).to.throw(message);
+      expect(function () { return new StoneArray({}); }).to.throw(message);
+    });
+
+    it('returns a new empty List object', function () {
+      const array = new StoneArray([]);
+
+      expect(array).to.eql([]);
+      expect(Array.isArray(array)).to.be.true();
+      expect(array).to.be.instanceOf(StoneArray);
+    });
+
+    it('returns a new List with same keys as the given array', function () {
+      const array = new StoneArray(['alf', 'mork']);
+
+      expect(array).to.eql(['alf', 'mork']);
+      expect(array).to.be.instanceOf(StoneArray);
+    });
+
+    it('returns nested immutable structures all the way down', function () {
+      const array = new StoneArray(['alf', 'mork', ['and', 'mindy']]);
+
+      expect(array).to.eql(['alf', 'mork', ['and', 'mindy']]);
+      expect(array[2]).to.be.instanceOf(StoneArray);
+      expect(function () { array[3] = 'phone home'; }).to.throw(frozenError);
+    });
+
+    it('throws an error when using mutation methods', function () {
+      const array = new StoneArray(['alf', 'mork']);
+
+      expect(function () { array.push(5); }).to.throw(frozenError);
+    });
+
+    it('allows you to concat new stuff', function () {
+      const array = new StoneArray(['alf', 'mork']);
+
+      expect(array.concat(5)).to.eql(['alf', 'mork', 5]);
+    });
+
+    it('returns the same List when nested within an array', function () {
+      const mork = new StoneArray(['and', 'mindy']);
+      const array = new StoneArray(['alf', 'mork', mork]);
+
+      expect(array).to.eql(['alf', 'mork', ['and', 'mindy']]);
+      expect(array[2]).to.eq(mork);
+    });
+
+    xit('handles circular references');
+  });
+});
