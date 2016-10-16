979d7ddaaac1e5aa475481e223d9e4a99edac731
Robert Fletcher <lobatifricha@gmail.com>
2016-10-15 17:34:50 -0700

    eslint goodies

--- app/assets/javascripts/app_base.jsx
+++ app/assets/javascripts/app_base.jsx
@@ -3,6 +3,7 @@
 const React = require('react');
 
 module.exports = React.createClass({
+  displayName: 'QuestlogBase',
   propTypes: {children: React.PropTypes.object.isRequired},
   render() {
     return (
