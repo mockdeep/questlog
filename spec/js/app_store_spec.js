23432e504d5cf086630fb2e58608999bc40c401e
Robert Fletcher <lobatifricha@gmail.com>
2017-01-16 15:24:49 -0800

    add top level app store and merged reducer

---
+++ spec/js/app_store_spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import appStore from 'js/app_store';
+
+describe('appStore', () => {
+  it('behaves like a store', () => {
+    const expectedState = {};
+    const subSpy = jest.fn();
+
+    expect(appStore.getState()).toEqual(expectedState);
+
+    appStore.subscribe(subSpy);
+    appStore.dispatch({type: '@@redux/INIT'});
+    expect(subSpy).toHaveBeenCalled();
+  });
+});
