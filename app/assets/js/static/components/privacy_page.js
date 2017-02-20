a378d2f283efa4d9f3bc0d5d59141f2cdaf0d6af
Robert Fletcher <lobatifricha@gmail.com>
2017-02-19 17:30:02 -0800

    use stateless functions for static pages

--- app/assets/js/static/components/privacy_page.js
+++ app/assets/js/static/components/privacy_page.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const PrivacyPage = React.createClass({
-  render() {
-    return (
-      <div>
-        <p>{'I won\'t sell your data.'}</p>
-        <p>{'I will use your data to make Questlog better.'}</p>
-      </div>
-    );
-  },
-});
+function PrivacyPage() {
+  return (
+    <div>
+      <p>{'I won\'t sell your data.'}</p>
+      <p>{'I will use your data to make Questlog better.'}</p>
+    </div>
+  );
+}
 
 export default PrivacyPage;
