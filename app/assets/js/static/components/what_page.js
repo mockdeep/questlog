e3f20923f6010e3e147cf307da12db40fbc73fab
Robert Fletcher <lobatifricha@gmail.com>
2017-02-19 17:30:02 -0800

    use stateless functions for static pages

--- app/assets/js/static/components/what_page.js
+++ app/assets/js/static/components/what_page.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-const WhatPage = React.createClass({
-  render() {
-    return (
-      <div>
-        <h1>{'What\'s a Questlog?'}</h1>
-        <p>{'Wouldn\'t you like to know...'}</p>
-        <Link to='/privacy'>{'Privacy'}</Link>
-      </div>
-    );
-  },
-});
+function WhatPage() {
+  return (
+    <div>
+      <h1>{'What\'s a Questlog?'}</h1>
+      <p>{'Wouldn\'t you like to know...'}</p>
+      <Link to='/privacy'>{'Privacy'}</Link>
+    </div>
+  );
+}
 
 export default WhatPage;
