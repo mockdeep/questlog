803b8f944dc6cb6fa4a2bdf72e0453c9853ea897
Robert Fletcher <lobatifricha@gmail.com>
2017-03-16 09:37:38 -0700

    remove RestfulStore
    Just copy properties to subclasses. This should make it easier to remove
    what is unnecessary for each of them and convert them one-by-one to
    redux.

--- app/js/task/bulk_store.js
+++ app/js/task/bulk_store.js
@@ -1,12 +1,92 @@
 import request from 'js/_helpers/request';
 
-import RestfulStore from 'js/_common/restful_store';
 import TaskStore from 'js/task/store';
 
 const BulkTaskStore = {
-  ...RestfulStore,
-
+  models: [],
+  loaded: false,
   name: 'bulk_task',
+
+  subscribe(listener) {
+    this.listeners = this.listeners || [];
+    this.listeners.push(listener);
+
+    return function unsubscribe() {
+      const index = this.listeners.indexOf(listener);
+
+      this.listeners.splice(index, 1);
+    }.bind(this);
+  },
+
+  notifyListeners() {
+    if (!this.listeners) { return; }
+
+    this.listeners.forEach((listener) => { listener(); });
+  },
+
+  url() {
+    return `/${this.name}s`;
+  },
+
+  load() {
+    request({
+      method: 'get',
+      url: this.url(),
+      success: this.updateModels.bind(this),
+    });
+  },
+
+  unload() {
+    this.loaded = false;
+    this.notifyListeners();
+  },
+
+  updateModels(data) {
+    this.models = data[`${this.name}s`];
+    this.loaded = true;
+    this.notifyListeners();
+  },
+
+  getAll() {
+    let promise;
+
+    if (this.loaded) {
+      const data = {};
+
+      data[`${this.name}s`] = this.models;
+
+      promise = new Promise(function resolveNow(resolve) { resolve(data); });
+    } else {
+      promise = request({
+        method: 'get',
+        url: this.url(),
+        success: this.updateModels.bind(this),
+      });
+    }
+
+    return promise;
+  },
+
+  create(attrs) {
+    const data = {};
+
+    data[this.name] = attrs;
+
+    return request({
+      data,
+      url: this.url(),
+      method: 'post',
+      success: this.unload.bind(this),
+    });
+  },
+
+  destroy(id) {
+    return request({
+      url: `${this.url()}/${id}`,
+      method: 'delete',
+      success: this.unload.bind(this),
+    });
+  },
 
   update(attrs) {
     const data = {};
