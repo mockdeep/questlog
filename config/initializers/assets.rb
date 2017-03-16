27115097b9588a68b5f0a6a6f5eea4682bde0c7d
Robert Fletcher <lobatifricha@gmail.com>
2017-03-15 19:54:36 -0700

    try preventing sprockets from compiling js assets
    was unable to deploy to Heroku, wondering if this is related.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,5 @@
 Rails.application.config.assets.version = '1.2'
 Rails.application.config.assets.paths += [
-  # path to enable namespacing require paths, e.g.: `require('js/router')`
-  Rails.root.join('app', 'assets'),
   # paths for CSS assets in node_modules directory
   Rails.root.join('node_modules', 'bootstrap-sass', 'assets', 'stylesheets'),
 ]
