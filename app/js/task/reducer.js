e55ef0919116dcd68bc5fa957ea2af7483e38bf1
Robert Fletcher <lobatifricha@gmail.com>
2017-04-04 09:00:46 -0700

    Redux: add fetch action for tasks reducer
    This doesn't actually plug in anything, but starts the process of
    creating some infrastructure to replace the TagStore with the task
    reducer and tag selectors.

--- app/js/task/reducer.js
+++ app/js/task/reducer.js
@@ -1,4 +1,13 @@
+import {normalize, schema} from 'normalizr';
+
 import createBasicReducer from 'js/_common/basic_reducer';
+
+const taskSchema = new schema.Entity('tasks');
+const taskListSchema = new schema.Array(taskSchema);
+
+function estimateMinutes(task) {
+  return Math.floor((task.estimateSeconds || 1800) / 60);
+}
 
 function initStore() {
   return {newTask: {title: ''}};
@@ -12,8 +21,15 @@ function setNewTask(previousState, newTask) {
   return {...previousState, newTask};
 }
 
+function setTasks(previousState, taskData) {
+  const newTasks = taskData.map((task) => ({...task, estimateMinutes: estimateMinutes(task)}));
+
+  return {...previousState, ...normalize(newTasks, taskListSchema)};
+}
+
 const operations = {
   'task/INIT': initStore,
+  'task/SET': setTasks,
   'task/SET_NEW': setNewTask,
   'task/SET_AJAX_STATE': setAjaxState,
 };
