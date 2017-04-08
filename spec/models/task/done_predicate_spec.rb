56d2da27b61246578419a862f3d5e4be049edceb
Robert Fletcher <lobatifricha@gmail.com>
2017-04-08 11:08:52 -0700

    upgrade gems

--- spec/models/task/done_predicate_spec.rb
+++ spec/models/task/done_predicate_spec.rb
@@ -2,12 +2,12 @@ RSpec.describe Task, '#done?' do
 
   let(:task) { Task.new }
 
-  it 'it returns true when done_at is present' do
+  it 'returns true when done_at is present' do
     task.done_at = Time.now
     expect(task).to be_done
   end
 
-  it 'it returns false when done_at is nil' do
+  it 'returns false when done_at is nil' do
     expect(task).not_to be_done
   end
 
