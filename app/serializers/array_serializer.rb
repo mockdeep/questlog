c96a9db14054aa35d1511c47649f7a21493ed27b
Robert Fletcher <lobatifricha@gmail.com>
2017-04-15 09:50:07 -0700

    Serializers: remove unused root_name method

--- app/serializers/array_serializer.rb
+++ app/serializers/array_serializer.rb
@@ -2,16 +2,6 @@ class ArraySerializer
 
   include Serializeable
 
-  def self.root_name
-    caller
-      .grep(/_controller/)
-      .grep_v(/application_controller/)
-      .first
-      .match(/\/(\w+)_controller/)
-      .captures
-      .first
-  end
-
 private
 
   def serialize_object(array)
