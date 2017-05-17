4443cbdc79c924c5efe4ba2fcb8ca79c81c26790
Robert Fletcher <lobatifricha@gmail.com>
2017-05-17 08:46:35 -0700

    Redux: add task selector to get next task

---
+++ spec/javascript/task/selectors_spec.js
@@ -0,0 +1,110 @@
+import {getNextUndoneTask} from 'src/task/selectors';
+
+describe('getNextUndoneTask', () => {
+  it('returns the next undone task', () => {
+    const task1 = {id: 5, title: 'foo', releaseAt: 'blah', timeframe: null};
+    const task2 = {id: 10, title: 'blah', timeframe: null};
+    const state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task2);
+  });
+
+  it('returns next task according to timeframe', () => {
+    const task1 = {id: 5, title: 'foo', timeframe: 'week'};
+    let task2 = {id: 10, title: 'blah', timeframe: null};
+    let state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task1);
+
+    task2 = {...task2, timeframe: 'today'};
+    state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task2);
+
+    task2 = {...task2, releaseAt: 'blah'};
+    state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task1);
+  });
+
+  it('returns next task according to priority', () => {
+    const task1 = {id: 5, title: 'foo', priority: 3, timeframe: null};
+    let task2 = {id: 5, title: 'blah', priority: null, timeframe: null};
+    let state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task1);
+
+    task2 = {...task2, priority: 2};
+    state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task2);
+
+    task2 = {...task2, releaseAt: 'someday'};
+    state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task1);
+  });
+
+  it('returns next task according to position', () => {
+    const task1 = {id: 5, title: 'foo', position: 32, timeframe: null};
+    let task2 = {id: 5, title: 'blah', position: 35, timeframe: null};
+    let state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task1);
+
+    task2 = {...task2, position: 2};
+    state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task2);
+
+    task2 = {...task2, releaseAt: 'someday'};
+    state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task1);
+  });
+
+  it('returns next task sorted based on various attributes', () => {
+    const task1 = {
+      id: 5,
+      title: 'foo',
+      position: 32,
+      priority: null,
+      timeframe: 'today',
+    };
+    let task2 = {
+      id: 5,
+      title: 'blah',
+      position: 35,
+      priority: null,
+      timeframe: 'week',
+    };
+    let state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task1);
+
+    task2 = {...task2, position: 2};
+    state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task1);
+
+    task2 = {...task2, priority: 1};
+    state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task1);
+
+    task2 = {...task2, timeframe: 'today'};
+    state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task2);
+
+    task2 = {...task2, priority: null};
+    state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task2);
+
+    task2 = {...task2, position: 35};
+    state = {tasks: {byId: {5: task1, 10: task2}}};
+
+    expect(getNextUndoneTask(state)).toBe(task1);
+  });
+});
