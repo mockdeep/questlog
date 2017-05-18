b71f3fe4879206017232136105152020fa6bc937
Robert Fletcher <lobatifricha@gmail.com>
2017-05-18 09:40:42 -0700

    Redux: small refactors in selectors

--- app/javascript/src/tag/selectors.js
+++ app/javascript/src/tag/selectors.js
@@ -1,15 +1,11 @@
 import {createSelector} from 'reselect';
 
-function getTagsFromState(state) {
-  return state.tag;
-}
-
 function isActive(tag) {
   return tag.unfinishedTasksCount > 0;
 }
 
-function orderTags(tags) {
-  return tags.orderedIds.map((tagId) => tags.byId[tagId]);
+function orderTags(tagState) {
+  return tagState.orderedIds.map((tagId) => tagState.byId[tagId]);
 }
 
 function filterActiveTags(orderedTags) {
@@ -20,8 +16,8 @@ function filterSelectedTags(getOrderedTags) {
   return getOrderedTags.filter((tag) => tag.isSelected);
 }
 
-const getOrderedTags = createSelector([getTagsFromState], orderTags);
-const getActiveTags = createSelector([getOrderedTags], filterActiveTags);
-const getSelectedTags = createSelector([getOrderedTags], filterSelectedTags);
+const getOrderedTags = createSelector((state) => state.tag, orderTags);
+const getActiveTags = createSelector(getOrderedTags, filterActiveTags);
+const getSelectedTags = createSelector(getOrderedTags, filterSelectedTags);
 
 export {getActiveTags, getSelectedTags};
