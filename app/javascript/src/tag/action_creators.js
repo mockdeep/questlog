2c6013e46fdf05680eccb5933eb45ea9d02b118b
Robert Fletcher <lobatifricha@gmail.com>
2017-05-19 09:23:46 -0700

    Redux: remove unused fetchTags action_creator
    This data is side-loaded when fetching tasks.

--- app/javascript/src/tag/action_creators.js
+++ app/javascript/src/tag/action_creators.js
@@ -1,17 +1,5 @@
-import request from 'src/_helpers/request';
-
 function setTags(payload) {
   return {type: 'tag/SET', payload};
 }
 
-function fetchTags() {
-  return (dispatch) => {
-    request({
-      method: 'get',
-      url: '/tags',
-      success: ({data}) => { dispatch(setTags(data)); },
-    });
-  };
-}
-
-export {setTags, fetchTags};
+export {setTags};
