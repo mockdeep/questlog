c847f05034d11c7e6ef2b9ba3922d43cfdc2cdc8
Robert Fletcher <lobatifricha@gmail.com>
2017-05-27 14:57:21 -0700

    Redux: tidy up action names in specs

--- spec/javascript/tag/reducer_spec.js
+++ spec/javascript/tag/reducer_spec.js
@@ -1,14 +1,15 @@
+import {INIT, SET, UPDATE, UPDATE_META} from 'src/tag/action_creators';
 import tagReducer from 'src/tag/reducer';
 
-describe('tag/INIT', () => {
+describe(INIT, () => {
   it('sets up the basic structure for the store', () => {
     const expected = {orderedIds: [], byId: {}};
 
-    expect(tagReducer(null, {type: 'tag/INIT'})).toEqual(expected);
+    expect(tagReducer(null, {type: INIT})).toEqual(expected);
   });
 });
 
-describe('tag/SET', () => {
+describe(SET, () => {
   it('replaces the existing tags', () => {
     const previousState = {
       meta: 'foo',
@@ -17,7 +18,7 @@ describe('tag/SET', () => {
     };
     const tag1 = {id: 1, name: 'a tag'};
     const tag2 = {id: 5, name: 'wat tag'};
-    const action = {type: 'tag/SET', payload: [tag2, tag1]};
+    const action = {type: SET, payload: [tag2, tag1]};
     const expected = {
       meta: 'foo',
       byId: {1: tag1, 5: tag2},
@@ -28,11 +29,11 @@ describe('tag/SET', () => {
   });
 });
 
-describe('tag/UPDATE', () => {
+describe(UPDATE, () => {
   it('throws an error when tag is missing from state', () => {
     const previousState = {byId: {}};
     const payload = {id: 1, bloo: 'blargh'};
-    const action = {type: 'tag/UPDATE', payload};
+    const action = {type: UPDATE, payload};
     const expectedError = 'no tag found for id 1';
 
     expect(() => { tagReducer(previousState, action); }).toThrow(expectedError);
@@ -44,7 +45,7 @@ describe('tag/UPDATE', () => {
       orderedIds: [1, 2],
     };
     const payload = {id: 1, bloo: 'blargh'};
-    const action = {type: 'tag/UPDATE', payload};
+    const action = {type: UPDATE, payload};
     const expectedState = {
       byId: {1: {id: 1, goo: 'ber', bloo: 'blargh'}, 2: {id: 2, bar: 'butz'}},
       orderedIds: [1, 2],
@@ -54,10 +55,10 @@ describe('tag/UPDATE', () => {
   });
 });
 
-describe('tag/UPDATE_META', () => {
+describe(UPDATE_META, () => {
   it('updates meta information in the state', () => {
     const previousState = {meta: {foo: 'bar', baz: 'butz'}};
-    const action = {type: 'tag/UPDATE_META', payload: {baz: 'bootz'}};
+    const action = {type: UPDATE_META, payload: {baz: 'bootz'}};
     const expectedState = {meta: {foo: 'bar', baz: 'bootz'}};
 
     expect(tagReducer(previousState, action)).toEqual(expectedState);
