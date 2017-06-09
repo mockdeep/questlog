43dde7d992bf757c2c1563965b70900be334749b
Robert Fletcher <lobatifricha@gmail.com>
2017-06-09 09:26:52 -0700

    Redux: switch item component to redux store
    Finally managed to rip out the old store after loads of backtracking.

--- app/javascript/src/task/containers/item.js
+++ app/javascript/src/task/containers/item.js
@@ -1,28 +1,20 @@
 import {connect} from 'react-redux';
 
-import {
-  deleteTask,
-  fetchTasks,
-  updateTask,
-  updateTaskMeta,
-} from 'src/task/action_creators';
+import {deleteTask, updateTask, updateTaskMeta} from 'src/task/action_creators';
 import {getActiveTags} from 'src/tag/selectors';
+import {getNextUndoneTask} from 'src/task/selectors';
 import {updateTagMeta} from 'src/tag/action_creators';
 import TaskItem from 'src/task/components/item';
 
 function mapStateToProps(state) {
   return {
     tags: getActiveTags(state),
+    task: getNextUndoneTask(state),
     postponeSeconds: state.task.meta.postponeSeconds,
+    ajaxState: state.task.meta.ajaxState,
   };
 }
 
-const actionCreators = {
-  deleteTask,
-  fetchTasks,
-  updateTagMeta,
-  updateTask,
-  updateTaskMeta,
-};
+const actionCreators = {deleteTask, updateTagMeta, updateTask, updateTaskMeta};
 
 export default connect(mapStateToProps, actionCreators)(TaskItem);
