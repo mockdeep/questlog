c861ce8d7eee39b6613a8cf35676ccea6892f6db
Robert Fletcher <lobatifricha@gmail.com>
2017-07-03 23:51:36 +0800

    Redux: create router container class
    More housekeeping in prep for removing React Router. I've got a side
    branch with more or less the final product, but getting file moves and
    minor refactors off that branch will help in keeping the more
    substantive changes manageable, not to mention making spec failures
    easier to source.

--- app/javascript/src/app_base.js
+++ app/javascript/src/app_base.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
 
 import appStore from 'src/app_store';
-import routes from 'src/_config/routes';
+import RouterContainer from 'src/route/containers/router';
 import {fetchTasks} from 'src/task/action_creators';
 
 appStore.dispatch(fetchTasks());
 
-const appBase =
-  <Provider store={appStore}>
-    <BrowserRouter>{routes}</BrowserRouter>
-  </Provider>
+const AppBase = <Provider store={appStore}><RouterContainer /></Provider>;
 
-;
-
-export default appBase;
+export default AppBase;
