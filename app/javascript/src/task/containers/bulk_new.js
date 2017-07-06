dfb59baab1e5dbe5aef776cf62de7e8c55eef022
Robert Fletcher <lobatifricha@gmail.com>
2017-07-06 13:42:21 +0800

    Redux: store bulk task temp task in store meta

--- app/javascript/src/task/containers/bulk_new.js
+++ app/javascript/src/task/containers/bulk_new.js
@@ -2,5 +2,12 @@ import {connect} from 'react-redux';
 
 import BulkTasksNew from 'src/task/components/bulk_new';
 import {setRoute} from 'src/route/action_creators';
+import {updateTaskMeta} from 'src/task/action_creators';
 
-export default connect(null, {setRoute})(BulkTasksNew);
+function mapStateToProps(state) {
+  return {taskTitles: state.task.meta.newTask.title};
+}
+
+const actionCreators = {setRoute, updateTaskMeta};
+
+export default connect(mapStateToProps, actionCreators)(BulkTasksNew);
