3e93fe62223d4934fd7ab0a7303c824d33df0d2c
Robert Fletcher <lobatifricha@gmail.com>
2017-07-16 09:55:39 -0700

    TestQuality: satisfy Jest coverage and bump some more

---
+++ spec/javascript/static/components/privacy_page_spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import PrivaceyPage from 'src/static/components/privacy_page';
+
+it('renders the privacy page', () => {
+  const component = shallow(<PrivaceyPage />);
+
+  expect(component.text()).toMatch(/I won't sell your data/);
+});
