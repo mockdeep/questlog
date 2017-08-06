ebdeb3dec1e1beab7ffe16c248ca0b806aab94d0
Robert Fletcher <lobatifricha@gmail.com>
2017-08-06 10:25:46 -0700

    make debug helper a little more helpful

--- app/javascript/src/_helpers/debug.js
+++ app/javascript/src/_helpers/debug.js
@@ -1,5 +1,17 @@
-function debug(result) {
-  console.log('debug --> ', result);
+function debug(...args) {
+  let result;
+  let message;
+
+  if (args.length === 2) {
+    result = args[1];
+    message = `debug (${args[0]})`;
+  } else {
+    result = args[0];
+    message = 'debug';
+  }
+
+  // eslint-disable-next-line no-console
+  console.log(`${message} --> `, result);
 
   return result;
 }
