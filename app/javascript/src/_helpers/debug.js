d1b24152bb9077f8c0308ba1917d12a902996927
Robert Fletcher <lobatifricha@gmail.com>
2017-08-13 09:54:04 -0700

    pretty up debug helper
    Added colorful labels for both browser and node.

--- app/javascript/src/_helpers/debug.js
+++ app/javascript/src/_helpers/debug.js
@@ -1,17 +1,82 @@
-function debug(...args) {
-  let result;
-  let message;
+function isNode() {
+  return typeof process !== 'undefined' && !process.browser;
+}
 
-  if (args.length === 2) {
-    result = args[1];
-    message = `debug (${args[0]})`;
+const terminalEscapes = {
+  magentaBackground: '\x1b[45m',
+  lightBlueForeground: '\x1b[94m',
+  bold: '\x1b[1m',
+  reset: '\x1b[0m',
+};
+
+const terminalStyles = {
+  debugStyle: `${terminalEscapes.magentaBackground}${terminalEscapes.bold}`,
+  messageStyle: terminalEscapes.lightBlueForeground,
+  arrowStyle: terminalEscapes.bold,
+  reset: terminalEscapes.reset,
+};
+
+const browserStyles = {
+  debugStyle: 'background-color: magenta; color: white',
+  messageStyle: 'color: blue',
+  arrowStyle: 'font-weight: bold',
+  reset: '',
+};
+
+function debugNode({result, userLabel}) {
+  const {debugStyle, messageStyle, arrowStyle, reset} = terminalStyles;
+  let label;
+
+  const debugLabel = `${debugStyle}  DEBUG  ${reset}`;
+
+  if (userLabel) {
+    label = `${debugLabel} (${messageStyle}${userLabel}${reset})`;
   } else {
-    result = args[0];
-    message = 'debug';
+    label = debugLabel;
   }
 
+  const arrow = `${arrowStyle}==>${reset}`;
+
   // eslint-disable-next-line no-console
-  console.log(`${message} --> `, result);
+  console.log(`${label} ${arrow} `, result);
+
+  return result;
+}
+
+function debugBrowser({result, userLabel}) {
+  const {debugStyle, messageStyle, arrowStyle, reset} = browserStyles;
+  const consoleArgs = [debugStyle, reset];
+  const debugLabel = '%c  DEBUG  %c';
+  let label;
+
+  if (userLabel) {
+    label = `${debugLabel} (%c${userLabel}%c)`;
+    consoleArgs.push(messageStyle, reset);
+  } else {
+    label = debugLabel;
+  }
+
+  const arrow = '%c==>%c';
+
+  consoleArgs.push(arrowStyle, reset);
+
+  // eslint-disable-next-line no-console
+  console.log(`${label} ${arrow}`, ...consoleArgs, result);
+}
+
+function debug(...args) {
+  let result;
+  let userLabel;
+
+  if (args.length === 2) {
+    [userLabel, result] = args;
+  } else {
+    [result] = args;
+  }
+
+  const debugMethod = isNode() ? debugNode : debugBrowser;
+
+  debugMethod({result, userLabel});
 
   return result;
 }
