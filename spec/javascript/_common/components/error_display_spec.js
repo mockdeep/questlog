4597459735e6f424ace373f5278d9707dbd61a66
Robert Fletcher <lobatifricha@gmail.com>
2017-08-17 08:11:44 -0700

    TestQuality: shore up js test coverage
    Fix a couple of minor issues in code.

---
+++ spec/javascript/_common/components/error_display_spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import ErrorDisplay from 'src/_common/components/error_display';
+
+const renderOpts = {lifecycleExperimental: true};
+
+it('renders the given errors', () => {
+  const errors = ['one error', 'two error'];
+  const component = shallow(<ErrorDisplay errors={errors} />, renderOpts);
+
+  const errorMessages = component.find('.error-messages li');
+
+  expect(errorMessages).toHaveLength(2);
+  expect(errorMessages.at(0).text()).toBe('one error');
+  expect(errorMessages.at(1).text()).toBe('two error');
+});
---
+++ spec/javascript/_helpers/debug_spec.js
@@ -0,0 +1,75 @@
+/* eslint-disable no-console */
+import debug from 'src/_helpers/debug';
+
+beforeEach(() => {
+  jest.spyOn(console, 'log').mockImplementation(() => { /* nada */ });
+});
+describe('when run from node', () => {
+  it('prints helpful debug output without a label', () => {
+    const myObject = {myKey: 'myValue'};
+
+    debug(myObject);
+
+    const expectedLabel = '\x1b[45m\x1b[1m  DEBUG  \x1b[0m \x1b[1m==>\x1b[0m ';
+
+    expect(console.log).toHaveBeenCalledWith(expectedLabel, myObject);
+  });
+
+  it('prints helpful debug output with a label', () => {
+    const myObject = {myKey: 'myValue'};
+
+    debug('my debug message', myObject);
+
+    const expectedLabel = '\x1b[45m\x1b[1m  DEBUG  \x1b[0m ' +
+      '(\x1b[94mmy debug message\x1b[0m) \x1b[1m==>\x1b[0m ';
+
+    expect(console.log).toHaveBeenCalledWith(expectedLabel, myObject);
+  });
+});
+
+describe('when in the browser', () => {
+  beforeEach(() => {
+    process.browser = true;
+  });
+
+  afterEach(() => {
+    delete process.browser;
+  });
+
+  it('prints helpful debug output without a label', () => {
+    const myObject = {myKey: 'myValue'};
+
+    debug(myObject);
+
+    const expectedLabel = '%c  DEBUG  %c %c==>%c';
+    const expectedStyles = [
+      'background-color: magenta; color: white',
+      '',
+      'font-weight: bold',
+      '',
+    ];
+
+    const expectedArgs = [expectedLabel, ...expectedStyles, myObject];
+
+    expect(console.log).toHaveBeenCalledWith(...expectedArgs);
+  });
+
+  it('prints helpful debug output with a label', () => {
+    const myObject = {myKey: 'myValue'};
+
+    debug('my debug message', myObject);
+
+    const expectedLabel = '%c  DEBUG  %c (%cmy debug message%c) %c==>%c';
+    const expectedStyles = [
+      'background-color: magenta; color: white',
+      '',
+      'color: blue',
+      '',
+      'font-weight: bold',
+      '',
+    ];
+    const expectedArgs = [expectedLabel, ...expectedStyles, myObject];
+
+    expect(console.log).toHaveBeenCalledWith(...expectedArgs);
+  });
+});
