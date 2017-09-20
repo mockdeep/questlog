5c38b9813f605ccf426d1c984bb83b8914fc2f90
Robert Fletcher <lobatifricha@gmail.com>
2017-09-19 20:18:52 -0700

    pull a couple of constants out of react class

--- app/javascript/src/task/components/postpone_button.js
+++ app/javascript/src/task/components/postpone_button.js
@@ -9,6 +9,24 @@ function isPostponing(task) {
   return task.loadingState === 'postponing';
 }
 
+const selectOptionsOptions = [
+  {value: '300', content: '5 minutes'},
+  {value: '1800', content: '30 minutes'},
+  {value: '3600', content: '1 hour'},
+  {value: '10800', content: '3 hours'},
+  {value: '21600', content: '6 hours'},
+  {value: '32400', content: '9 hours'},
+  {value: '43200', content: '12 hours'},
+  {value: '86400', content: '1 day'},
+  {value: '172800', content: '2 days'},
+  {value: '259200', content: '3 days'},
+  {value: '604800', content: '1 week'},
+  {value: '1209600', content: '2 weeks'},
+  {value: '2592000', content: '1 month'},
+];
+
+const className = 'btn btn-info btn-lg btn-block postpone-button';
+
 const PostponeButton = createReactClass({
   displayName: 'PostponeButton',
 
@@ -24,24 +42,8 @@ const PostponeButton = createReactClass({
     this.props.postponeTask(this.props.task.id);
   },
 
-  selectOptionsOptions: [
-    {value: '300', content: '5 minutes'},
-    {value: '1800', content: '30 minutes'},
-    {value: '3600', content: '1 hour'},
-    {value: '10800', content: '3 hours'},
-    {value: '21600', content: '6 hours'},
-    {value: '32400', content: '9 hours'},
-    {value: '43200', content: '12 hours'},
-    {value: '86400', content: '1 day'},
-    {value: '172800', content: '2 days'},
-    {value: '259200', content: '3 days'},
-    {value: '604800', content: '1 week'},
-    {value: '1209600', content: '2 weeks'},
-    {value: '2592000', content: '1 month'},
-  ],
-
   selectOptions() {
-    return this.selectOptionsOptions.map(this.selectOption);
+    return selectOptionsOptions.map(this.selectOption);
   },
 
   selectOption(optionOptions) {
@@ -53,8 +55,6 @@ const PostponeButton = createReactClass({
       />
     );
   },
-
-  className: 'btn btn-info btn-lg btn-block postpone-button',
 
   storePostponeSeconds(event) {
     this.props.storePostponeSeconds(parseInt(event.target.value, 10));
@@ -69,7 +69,7 @@ const PostponeButton = createReactClass({
       <div
         id='postpone'
         disabled={this.props.disabled}
-        className={this.className}
+        className={className}
         onClick={this.postponeTask}
       >
         <label>{this.buttonMessage()}</label>
