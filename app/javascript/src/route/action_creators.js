75909239fb0ead826053e3ec3c835c85d40f45a6
Robert Fletcher <lobatifricha@gmail.com>
2017-09-22 20:31:01 -0700

    Redux: move route finding logic out of action creators
    Seems like a better location for it all.

--- app/javascript/src/route/action_creators.js
+++ app/javascript/src/route/action_creators.js
@@ -1,28 +1,15 @@
-import ROUTES, {findRoute} from 'src/route/routes';
+import {findRoute, matchPath} from 'src/route/routes';
 
 const INIT = 'route/INIT';
 const SET = 'route/SET';
 
 function fetchRoute() {
-  const path = window.location.pathname;
-  let params;
-
-  const matchingRoute = ROUTES.find((route) => {
-    params = route.match(path);
-
-    return Boolean(params);
-  });
-
-  if (!matchingRoute) { throw new Error(`No route found for path: ${path}`); }
-
-  return {type: SET, payload: {name: matchingRoute.name, params}};
+  return {type: SET, payload: matchPath(window.location.pathname)};
 }
 
 function setRoute(payload) {
   const {name, ...params} = payload;
   const matchingRoute = findRoute(name);
-
-  if (!matchingRoute) { throw new Error(`No route found for name: ${name}`); }
 
   window.history.pushState(null, null, matchingRoute.toPath(params));
 
---
+++ spec/javascript/route/routes_spec.js
@@ -0,0 +1,65 @@
+import {findRoute, matchPath} from 'src/route/routes';
+
+describe('findRoute', () => {
+  it('returns a root route', () => {
+    const route = findRoute('root');
+
+    expect(route.name).toBe('root');
+    expect(route.toPath()).toBe('/');
+  });
+
+  it('returns a basic route', () => {
+    const route = findRoute('tasks');
+
+    expect(route.name).toBe('tasks');
+    expect(route.toPath()).toBe('/tasks');
+  });
+
+  it('returns a route that can be compiled with a param', () => {
+    const route = findRoute('tag');
+
+    expect(route.name).toBe('tag');
+    expect(route.toPath({slug: 'foo'})).toBe('/foo');
+  });
+
+  it('returns a nested route', () => {
+    const route = findRoute('sessionsNew');
+
+    expect(route.name).toBe('sessionsNew');
+    expect(route.toPath()).toBe('/sessions/new');
+  });
+
+  it('raises an error when a route cannot be found', () => {
+    expect(() => { findRoute('booRoute'); }).toThrow(/No route found/);
+  });
+});
+
+describe('matchPath', () => {
+  it('returns a match for a root route', () => {
+    const match = matchPath('/');
+
+    expect(match).toEqual({name: 'root', params: {}});
+  });
+
+  it('returns a match for a basic route', () => {
+    const match = matchPath('/tasks');
+
+    expect(match).toEqual({name: 'tasks', params: {}});
+  });
+
+  it('returns a match with a route param', () => {
+    const match = matchPath('/foo');
+
+    expect(match).toEqual({name: 'tag', params: {slug: 'foo'}});
+  });
+
+  it('returns a match with a nested route', () => {
+    const match = matchPath('/sessions/new');
+
+    expect(match).toEqual({name: 'sessionsNew', params: {}});
+  });
+
+  it('raises an error when a match cannot be found', () => {
+    expect(() => { matchPath('/not/a/route'); }).toThrow(/No route found/);
+  });
+});
