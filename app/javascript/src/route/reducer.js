28d9fa7664155bb4386f16fd0be584ca97410d75
Robert Fletcher <lobatifricha@gmail.com>
2017-09-22 20:03:07 -0700

    Redux: move routing logic to action creators
    The reducer is supposed to be "pure", so shouldn't be fetching things
    from the url and throwing errors.

--- app/javascript/src/route/reducer.js
+++ app/javascript/src/route/reducer.js
@@ -1,31 +1,12 @@
 import createBasicReducer from 'src/_common/create_basic_reducer';
-import ROUTES, {findRoute} from 'src/route/routes';
 import {INIT, SET} from 'src/route/action_creators';
 
 export default createBasicReducer({
   [INIT]() {
-    const path = window.location.pathname;
-    let params;
-
-    const matchingRoute = ROUTES.find((route) => {
-      params = route.match(path);
-
-      return Boolean(params);
-    });
-
-    if (!matchingRoute) { throw new Error(`No route found for path: ${path}`); }
-
-    return {name: matchingRoute.name, params};
+    return {params: {}};
   },
 
   [SET](previousState, payload) {
-    const {name, ...params} = payload;
-    const matchingRoute = findRoute(name);
-
-    if (!matchingRoute) { throw new Error(`No route found for name: ${name}`); }
-
-    window.history.pushState(null, null, matchingRoute.toPath(params));
-
-    return {name: matchingRoute.name, params};
+    return payload;
   },
 });
--- spec/javascript/route/reducer_spec.js
+++ spec/javascript/route/reducer_spec.js
@@ -2,42 +2,17 @@ import {INIT, SET} from 'src/route/action_creators';
 import routeReducer from 'src/route/reducer';
 
 describe(INIT, () => {
-  it('returns a route based on the current browser URL', () => {
+  it('returns basic route state', () => {
     const action = {type: INIT};
 
-    expect(routeReducer({}, action)).toEqual({name: 'root', params: {}});
-
-    window.history.replaceState(null, null, '/tasks');
-
-    expect(routeReducer({}, action)).toEqual({name: 'tasks', params: {}});
-
-    window.history.replaceState(null, null, '/my-house');
-
-    const expectedState = {name: 'tag', params: {slug: 'my-house'}};
-
-    expect(routeReducer({}, action)).toEqual(expectedState);
-
-    window.history.replaceState(null, null, '/sessions/new');
-
-    expect(routeReducer({}, action)).toEqual({name: 'sessionsNew', params: {}});
+    expect(routeReducer({}, action)).toEqual({params: {}});
   });
 });
 
 describe(SET, () => {
-  it('returns a route object corresponding to the given payload', () => {
-    const action = {type: SET, payload: {name: 'what', foo: 'bar'}};
-    const previousState = {name: 'blah', params: {some: 'thing'}};
-    const expectedState = {name: 'what', params: {foo: 'bar'}};
+  it('returns route object corresponding to the given payload', () => {
+    const action = {type: SET, payload: {goober: 'gabber'}};
 
-    expect(routeReducer(previousState, action)).toEqual(expectedState);
-  });
-
-  it('updates the browser URL', () => {
-    const action = {type: SET, payload: {name: 'what', foo: 'bar'}};
-    const previousState = {name: 'blah', params: {some: 'thing'}};
-
-    routeReducer(previousState, action);
-
-    expect(window.location.pathname).toBe('/what');
+    expect(routeReducer({}, action)).toBe(action.payload);
   });
 });
