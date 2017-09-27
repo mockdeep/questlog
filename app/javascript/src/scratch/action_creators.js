c1cc323aa703eb95f7c51004967f09c543502d1d
Robert Fletcher <lobatifricha@gmail.com>
2017-09-26 18:26:46 -0700

    Redux: moving errors out of scratch reducer
    reducers should be pure and not raise errors. Totally cool to raise from
    the action creators, though!

--- app/javascript/src/scratch/action_creators.js
+++ app/javascript/src/scratch/action_creators.js
@@ -3,17 +3,48 @@ const CREATE = 'scratch/CREATE';
 const DELETE = 'scratch/DELETE';
 const UPDATE = 'scratch/UPDATE';
 
+function createScratchPlain(payload) {
+  return {type: CREATE, payload};
+}
+
+function deleteScratchPlain(payload) {
+  return {type: DELETE, payload};
+}
+
+function updateScratchPlain(payload) {
+  return {type: UPDATE, payload};
+}
+
 function createScratch(scratchKey) {
-  return {type: CREATE, payload: scratchKey};
+  return function createScratchThunk(dispatch, getState) {
+    if (Object.prototype.hasOwnProperty.call(getState().scratch, scratchKey)) {
+      throw new Error(`Scratch state already has key: "${scratchKey}"`);
+    }
+
+    dispatch(createScratchPlain(scratchKey));
+  };
 }
 
 function deleteScratch(scratchKey) {
-  return {type: DELETE, payload: scratchKey};
+  return function deleteScratchThunk(dispatch, getState) {
+    if (!Object.prototype.hasOwnProperty.call(getState().scratch, scratchKey)) {
+      throw new Error(`Scratch has no such key to delete: "${scratchKey}"`);
+    }
+
+    dispatch(deleteScratchPlain(scratchKey));
+  };
 }
 
 function updateScratch(scratchKey, payload) {
-  return {type: UPDATE, payload: {key: scratchKey, ...payload}};
+  return function updateScratchThunk(dispatch, getState) {
+    if (!Object.prototype.hasOwnProperty.call(getState().scratch, scratchKey)) {
+      throw new Error(`Scratch has no such key to update: "${scratchKey}"`);
+    }
+
+    dispatch(updateScratchPlain({key: scratchKey, ...payload}));
+  };
 }
 
 export {INIT, CREATE, DELETE, UPDATE};
+export {createScratchPlain, deleteScratchPlain, updateScratchPlain};
 export {createScratch, deleteScratch, updateScratch};
