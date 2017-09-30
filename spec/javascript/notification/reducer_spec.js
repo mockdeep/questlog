e349d0c20df4cd9318214e0d7500fa5cd2684b0d
Rokkside <rokkside@gmail.com>
2017-09-30 08:09:17 -0700

    New modification ADD test for notification reducer (#3)

--- spec/javascript/notification/reducer_spec.js
+++ spec/javascript/notification/reducer_spec.js
@@ -1,4 +1,4 @@
-import {INIT} from 'src/notification/action_creators';
+import {INIT, ADD} from 'src/notification/action_creators';
 import notificationReducer from 'src/notification/reducer';
 
 describe(INIT, () => {
@@ -6,5 +6,16 @@ describe(INIT, () => {
     const expected = {};
 
     expect(notificationReducer(null, {type: INIT})).toEqual(expected);
+  });
+});
+
+describe(ADD, () => {
+  it('returns a new object with a payload merged in', () => {
+    const someKey = Math.random();
+    const previousState = {[someKey]: Math.random()};
+    const action = {type: ADD, payload: {key: 'foo', notification: 'bar'}};
+    const expected = {[someKey]: previousState[someKey], foo: 'bar'};
+
+    expect(notificationReducer(previousState, action)).toEqual(expected);
   });
 });
