f5534a870db1e8b7e51360c557e991725bb65303
Robert Fletcher <lobatifricha@gmail.com>
2017-10-05 20:10:08 -0700

    Chores: upgrade to React 16
    - had to use `immutability-helper` in `list.js` as React no longer
      provides update helper
    - React now requires `requestAnimationFrame`, so shimmed that out.
      Pulling in the `enzyme` adapter requires in `React`, which gives a
      warning, so needed to put it in a `shims.js` file that is imported
      first. Also moved the `console.error` shim up as well, since warnings
      from React were giving no trace.

--- app/javascript/src/task/components/list.js
+++ app/javascript/src/task/components/list.js
@@ -1,7 +1,7 @@
 import autobind from 'class-autobind';
 import PropTypes from 'prop-types';
 import React from 'react';
-import update from 'react/lib/update';
+import update from 'immutability-helper';
 import {partition} from 'lodash';
 import HTML5Backend from 'react-dnd-html5-backend';
 import {DragDropContext as dragDropContext} from 'react-dnd';
