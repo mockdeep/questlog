8dab2e21d1a570afd31a883228894f9c27a9daa6
Robert Fletcher <lobatifricha@gmail.com>
2017-10-15 18:25:58 -0700

    Design: un-center document body
    Feedback from Melissa. Instead default to left aligned and center
    selectively.

--- app/javascript/src/session/components/new_form.js
+++ app/javascript/src/session/components/new_form.js
@@ -4,30 +4,32 @@ import authenticityToken from 'src/_helpers/authenticity_token';
 
 function NewSessionForm() {
   return (
-    <div>
+    <form
+      action='/session'
+      acceptCharset='UTF-8'
+      method='post'
+      className='login-form'
+    >
       <h1>{'Log in'}</h1>
-
-      <form action='/session' acceptCharset='UTF-8' method='post'>
-        <input
-          type='hidden'
-          name='authenticity_token'
-          value={authenticityToken()}
-        />
-        <p>
-          <label htmlFor='email'>{'Email'}</label>
-          <br />
-          <input type='text' name='session[email]' id='email' />
-        </p>
-        <p>
-          <label htmlFor='password'>{'Password'}</label>
-          <br />
-          <input type='password' name='session[password]' id='password' />
-        </p>
-        <p className='button'>
-          <input type='submit' name='commit' value='Login' />
-        </p>
-      </form>
-    </div>
+      <input
+        type='hidden'
+        name='authenticity_token'
+        value={authenticityToken()}
+      />
+      <p>
+        <label htmlFor='email'>{'Email'}</label>
+        <br />
+        <input type='text' name='session[email]' id='email' />
+      </p>
+      <p>
+        <label htmlFor='password'>{'Password'}</label>
+        <br />
+        <input type='password' name='session[password]' id='password' />
+      </p>
+      <p className='button'>
+        <input type='submit' name='commit' value='Login' />
+      </p>
+    </form>
   );
 }
 
