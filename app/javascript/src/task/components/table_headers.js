ade5110b3826f1e2721bb45f17ac475061d9dc54
Robert Fletcher <lobatifricha@gmail.com>
2017-10-15 17:50:37 -0700

    Design: merge headers into table
    Feedback from Melissa

---
+++ app/javascript/src/task/components/table_headers.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+function TableHeaders({label}) {
+  return (
+    <tr>
+      <th className='task-list__header' />
+      <th className='task-list__header'>{`${label} tasks`}</th>
+      <th className='task-list__header'>{'Estimate'}</th>
+      <th className='task-list__header' />
+      <th className='task-list__header'>{'Priority'}</th>
+      <th className='task-list__header' />
+      <th className='task-list__header' />
+    </tr>
+  );
+}
+
+TableHeaders.propTypes = {label: PropTypes.string.isRequired};
+
+export default TableHeaders;
