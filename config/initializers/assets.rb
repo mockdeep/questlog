c0ec94df261f3cdc5828c16c13261a975381610a
Robert Fletcher <lobatifricha@gmail.com>
2017-10-15 19:19:54 -0700

    Design: add Roboto font for task actions
    Feedback from Melissa

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,6 @@
 Rails.application.config.assets.version = '1.2'
 Rails.application.config.assets.paths += [
+  Rails.root.join('vendor', 'assets', 'fonts'),
   # paths for CSS assets in node_modules directory
   Rails.root.join('node_modules', 'bootstrap-sass', 'assets', 'stylesheets'),
 ]
