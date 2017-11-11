37c6b81c1f47b490f521c6f3779289ddebf98c1f
Robert Fletcher <lobatifricha@gmail.com>
2017-11-10 18:41:58 -0800

    ChildTasks: add parent_id column to tasks

--- spec/models/task/associations_spec.rb
+++ spec/models/task/associations_spec.rb
@@ -1,6 +1,8 @@
 RSpec.describe Task, 'associations' do
 
+  it { is_expected.to belong_to(:parent) }
   it { is_expected.to belong_to(:user) }
+  it { is_expected.to have_many(:children) }
   it { is_expected.to have_many(:taggings).dependent(:destroy) }
   it { is_expected.to have_many(:tags).through(:taggings) }
 
