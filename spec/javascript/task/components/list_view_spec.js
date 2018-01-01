965293873c550385f50c9457bb5067f50ef472b0
Robert Fletcher <lobatifricha@gmail.com>
2018-01-01 12:34:49 -0800

    Specs: improve tests around `list_view`
    Discovered a way to use `shallow` instead of `mount` in tests like
    these: `dive()`. But that made a mess of the coverage so I had to write
    some more. Finally got around to testing the drag and drop logic, and
    what a mess.

--- spec/javascript/task/components/list_view_spec.js
+++ spec/javascript/task/components/list_view_spec.js
@@ -1,9 +1,10 @@
-jest.mock('src/task/store');
+jest.mock('src/task/bulk_store');
 
 import React from 'react';
-import {mount} from 'enzyme';
+import {shallow} from 'enzyme';
 import {Provider} from 'react-redux';
 
+import BulkTaskStore from 'src/task/bulk_store';
 import createAppStore from 'src/create_app_store';
 import TaskListView from 'src/task/components/list_view';
 
@@ -14,54 +15,170 @@ const props = {
   updateTask: jest.fn(),
 };
 
-function mountWithProvider(component) {
+const DraggableTaskRow = 'DropTarget(DragSource(DraggableTaskRow))';
+
+function shallowProvider(component) {
   const provider = (
     <Provider store={createAppStore()}>
       {component}
     </Provider>
   );
 
-  return mount(provider);
+  return shallow(provider).dive().shallow();
 }
 
 it('renders a new task form', () => {
-  const component = mountWithProvider(<TaskListView {...props} />);
+  const component = shallowProvider(<TaskListView {...props} />);
 
-  expect(component.find('NewTaskForm')).toBePresent();
+  expect(component.find('Connect(NewTaskForm)')).toBePresent();
 });
 
 it('renders current tasks', () => {
   const overrides = {currentTasks: [{id: 1}]};
 
-  const component = mountWithProvider(<TaskListView {...props} {...overrides} />);
+  const component = shallowProvider(<TaskListView {...props} {...overrides} />);
 
-  expect(component.find('DraggableTaskRow')).toHaveLength(1);
+  expect(component.find(DraggableTaskRow)).toHaveLength(1);
   const tableHeaders = component.find('TableHeaders');
   expect(tableHeaders).toHaveLength(1);
   expect(tableHeaders).toHaveProp('label', 'Current tasks');
 });
 
 it('does not render a current tasks table when none are present', () => {
-  const component = mountWithProvider(<TaskListView {...props} />);
+  const component = shallowProvider(<TaskListView {...props} />);
 
-  expect(component.find('DraggableTaskRow')).toHaveLength(0);
+  expect(component.find(DraggableTaskRow)).toHaveLength(0);
   expect(component.find('TableHeaders')).toHaveLength(0);
 });
 
 it('renders pending tasks', () => {
   const overrides = {pendingTasks: [{id: 1}]};
 
-  const component = mountWithProvider(<TaskListView {...props} {...overrides} />);
+  const component = shallowProvider(<TaskListView {...props} {...overrides} />);
 
-  expect(component.find('DraggableTaskRow')).toHaveLength(1);
+  expect(component.find(DraggableTaskRow)).toHaveLength(1);
   const tableHeaders = component.find('TableHeaders');
   expect(tableHeaders).toHaveLength(1);
   expect(tableHeaders).toHaveProp('label', 'Pending tasks');
 });
 
 it('does not render a pending tasks table when none are present', () => {
-  const component = mountWithProvider(<TaskListView {...props} />);
+  const component = shallowProvider(<TaskListView {...props} />);
 
-  expect(component.find('DraggableTaskRow')).toHaveLength(0);
+  expect(component.find(DraggableTaskRow)).toHaveLength(0);
   expect(component.find('TableHeaders')).toHaveLength(0);
+});
+
+it('updates task rows based on updated props', () => {
+  const overrides = {currentTasks: [{id: 1}]};
+
+  const component = shallowProvider(<TaskListView {...props} {...overrides} />);
+  component.setProps({currentTasks: [], pendingTasks: [{id: 1}]});
+
+  expect(component.find(DraggableTaskRow)).toHaveLength(1);
+  const tableHeaders = component.find('TableHeaders');
+  expect(tableHeaders).toHaveLength(1);
+  expect(tableHeaders).toHaveProp('label', 'Pending tasks');
+});
+
+it('moves a task after another task', () => {
+  const overrides = {currentTasks: [{id: 1}, {id: 52}]};
+  const component = shallowProvider(<TaskListView {...props} {...overrides} />);
+  let taskRows = component.find(DraggableTaskRow);
+  expect(taskRows).toHaveLength(2);
+  expect(taskRows.at(0)).toHaveProp('task', overrides.currentTasks[0]);
+  expect(taskRows.at(1)).toHaveProp('task', overrides.currentTasks[1]);
+
+  taskRows.at(0).prop('moveTask')(1, 52);
+  component.update();
+
+  taskRows = component.find(DraggableTaskRow);
+  expect(taskRows).toHaveLength(2);
+  expect(taskRows.at(0)).toHaveProp('task', overrides.currentTasks[1]);
+  expect(taskRows.at(1)).toHaveProp('task', overrides.currentTasks[0]);
+});
+
+describe('saving task after drop', () => {
+  it('sets task priority to match below task when moved to top', () => {
+    const task1 = {id: 1, priority: 2};
+    const task2 = {id: 2, priority: 3};
+    const task3 = {id: 3, priority: null};
+    const overrides = {currentTasks: [task3, task1, task2]};
+    const component = shallowProvider(<TaskListView {...props} {...overrides} />);
+    const updatePriority = jest.fn();
+    const fakeComponent = {props: {task: task3}, updatePriority};
+
+    component.instance().saveTaskPositions(fakeComponent);
+
+    expect(updatePriority).toHaveBeenCalledWith({target: {value: 2}});
+  });
+
+  it('sets task priority to match above task when moved to bottom', () => {
+    const task1 = {id: 1, priority: 2};
+    const task2 = {id: 2, priority: 3};
+    const task3 = {id: 3, priority: 3};
+    const overrides = {currentTasks: [task2, task3, task1]};
+    const component = shallowProvider(<TaskListView {...props} {...overrides} />);
+    const updatePriority = jest.fn();
+    const fakeComponent = {props: {task: task1}, updatePriority};
+
+    component.instance().saveTaskPositions(fakeComponent);
+
+    expect(updatePriority).toHaveBeenCalledWith({target: {value: 3}});
+  });
+
+  it('keeps task priority when below task matches but not above', () => {
+    const task1 = {id: 1, priority: 2};
+    const task2 = {id: 2, priority: 3};
+    const task3 = {id: 3, priority: 3};
+    const overrides = {currentTasks: [task1, task3, task2]};
+    const component = shallowProvider(<TaskListView {...props} {...overrides} />);
+    const updatePriority = jest.fn();
+    const fakeComponent = {props: {task: task3}, updatePriority};
+
+    component.instance().saveTaskPositions(fakeComponent);
+
+    expect(updatePriority).toHaveBeenCalledWith({target: {value: 3}});
+  });
+
+  it('keeps task priority when above task matches but not below', () => {
+    const task1 = {id: 1, priority: 2};
+    const task2 = {id: 2, priority: 2};
+    const task3 = {id: 3, priority: 3};
+    const overrides = {currentTasks: [task2, task1, task3]};
+    const component = shallowProvider(<TaskListView {...props} {...overrides} />);
+    const updatePriority = jest.fn();
+    const fakeComponent = {props: {task: task1}, updatePriority};
+
+    component.instance().saveTaskPositions(fakeComponent);
+
+    expect(updatePriority).toHaveBeenCalledWith({target: {value: 2}});
+  });
+
+  it('sets task priority to below task priority when neither match', () => {
+    const task1 = {id: 1, priority: 1};
+    const task2 = {id: 2, priority: 2};
+    const task3 = {id: 3, priority: 3};
+    const overrides = {currentTasks: [task2, task1, task3]};
+    const component = shallowProvider(<TaskListView {...props} {...overrides} />);
+    const updatePriority = jest.fn();
+    const fakeComponent = {props: {task: task1}, updatePriority};
+
+    component.instance().saveTaskPositions(fakeComponent);
+
+    expect(updatePriority).toHaveBeenCalledWith({target: {value: 3}});
+  });
+
+  it('updates the tasks on the server', () => {
+    const task1 = {id: 1, priority: 1};
+    const task2 = {id: 2, priority: 2};
+    const task3 = {id: 3, priority: 3};
+    const overrides = {currentTasks: [task2, task1, task3]};
+    const component = shallowProvider(<TaskListView {...props} {...overrides} />);
+    const fakeComponent = {props: {task: task1}, updatePriority: jest.fn()};
+
+    component.instance().saveTaskPositions(fakeComponent);
+
+    expect(BulkTaskStore.update).toHaveBeenCalledWith({positions: [2, 1, 3]});
+  });
 });
---
+++ spec/javascript/task/components/new_task_form_spec.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import NewTaskForm from 'src/task/components/new_task_form';
+
+const createTask = jest.fn();
+const updateTaskMeta = jest.fn();
+const props = {
+  createTask,
+  task: {title: ''},
+  taskSaving: false,
+  updateTaskMeta,
+};
+
+it('sets the temporary task title on change', () => {
+  const component = shallow(<NewTaskForm {...props} />);
+  const fakeEvent = {target: {value: 'my text'}};
+
+  component.find("input[type='text']").simulate('change', fakeEvent);
+
+  expect(updateTaskMeta).toHaveBeenLastCalledWith({newTask: {title: 'my text'}});
+});
+
+it('saves the task on submit', () => {
+  const overrides = {task: {title: 'new title'}};
+  const component = shallow(<NewTaskForm {...props} {...overrides} />);
+  const preventDefault = jest.fn();
+  const fakeEvent = {preventDefault};
+
+  component.find('form').simulate('submit', fakeEvent);
+
+  expect(preventDefault).toHaveBeenCalled();
+  expect(createTask).toHaveBeenCalledWith({title: 'new title'});
+});
