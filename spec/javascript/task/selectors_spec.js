db089bdf920a498bdf635213e5142345ed0f913f
Robert Fletcher <lobatifricha@gmail.com>
2018-01-11 13:33:12 -0800

    Factories: switch task/selectors_spec to factories

--- spec/javascript/task/selectors_spec.js
+++ spec/javascript/task/selectors_spec.js
@@ -4,23 +4,19 @@ import {
   getUndoneTasks,
 } from 'src/task/selectors';
 
+import {makeState, makeTask} from '_test_helpers/factories';
+
 describe('getCurrentTask', () => {
   it('returns the current task represented in the route', () => {
-    const task = {id: 53, title: 'foo task'};
-    const state = {
-      task: {byId: {53: task}},
-      route: {params: {taskId: 53}},
-    };
+    const task = makeTask({title: 'foo task'});
+    const state = makeState({route: {params: {taskId: task.id}}, task: [task]});
 
-    expect(getCurrentTask(state)).toEqual({...task, subTaskIds: []});
+    expect(getCurrentTask(state)).toEqual(task);
   });
 
   it('returns no task when task in route is not present', () => {
-    const task = {id: 53, title: 'foo task'};
-    const state = {
-      task: {byId: {53: task}},
-      route: {params: {taskId: 54}},
-    };
+    const task = makeTask({title: 'foo task'});
+    const state = makeState({route: {params: {taskId: 0}}, task: [task]});
 
     expect(getCurrentTask(state)).toBeUndefined();
   });
@@ -28,56 +24,48 @@ describe('getCurrentTask', () => {
 
 describe('getUndoneTasks', () => {
   it('returns tasks', () => {
-    const task1 = {id: 53, title: 'some task', timeframe: null, subTaskIds: []};
-    const task2 = {id: 54, title: 'some other task', timeframe: null, subTaskIds: []};
-
-    const state = {task: {byId: {53: task1, 54: task2}}};
+    const task1 = makeTask({title: 'some task'});
+    const task2 = makeTask({title: 'some other task'});
+    const state = makeState({task: [task1, task2]});
 
     expect(getUndoneTasks(state)).toEqual([task1, task2]);
   });
 
   it('does not return tasks with a release date', () => {
-    const task1 = {
-      id: 53,
-      title: 'some task',
-      timeframe: null,
-      subTaskIds: [],
-      releaseAt: new Date(),
-    };
-    const task2 = {id: 54, title: 'some other task', timeframe: null, subTaskIds: []};
-
-    const state = {task: {byId: {53: task1, 54: task2}}};
+    const task1 = makeTask({title: 'some task', releaseAt: new Date()});
+    const task2 = makeTask({title: 'some other task'});
+    const state = makeState({task: [task1, task2]});
 
     expect(getUndoneTasks(state)).toEqual([task2]);
   });
 
   it('does not return tasks with sub tasks', () => {
-    const task1 = {id: 53, title: 'some task', timeframe: null};
-    const task2 = {id: 54, title: 'some other task', timeframe: null};
-    const task3 = {id: 55, title: 'some child task', timeframe: null, parentTaskId: 54};
-    const state = {task: {byId: {53: task1, 54: task2, 55: task3}}};
-    const expected = [{...task1, subTaskIds: []}, {...task3, subTaskIds: []}];
+    const task1 = makeTask({title: 'some task'});
+    const task2 = makeTask({title: 'some other task'});
+    const task3 = makeTask({title: 'some child task', parentTaskId: task2.id});
+    const state = makeState({task: [task1, task2, task3]});
 
-    expect(getUndoneTasks(state)).toEqual(expected);
+    expect(getUndoneTasks(state)).toEqual([task1, task3]);
   });
 
   it('raises an error when task has invalid timeframe', () => {
-    const task = {id: 3, title: 'a task', timeframe: 'yesterday', subTaskIds: []};
-
-    const state = {task: {byId: {3: task}}};
+    const task = makeTask({title: 'a task', timeframe: 'yesterday'});
+    const state = makeState({task: [task]});
 
     expect(() => getUndoneTasks(state)).toThrow(/object has no key "yesterday"/);
   });
 });
 
 describe('getPartitionedTasksForRoute', () => {
-  const subSubTask = {id: 10, timeframe: null, parentTaskId: 5, subTaskIds: []};
-  const subTask = {id: 5, timeframe: null, parentTaskId: 2, subTaskIds: [10]};
-  const parentTask = {id: 2, timeframe: null, subTaskIds: [5]};
-  const byId = {2: parentTask, 5: subTask, 10: subSubTask};
+  const parentTask = makeTask();
+  const subTask = makeTask({parentTaskId: parentTask.id});
+  const subSubTask = makeTask({parentTaskId: subTask.id});
+  parentTask.subTaskIds = [subTask.id];
+  subTask.subTaskIds = [subSubTask.id];
+  const tasks = [parentTask, subTask, subSubTask];
 
   it('returns all tasks when route is "/tasks"', () => {
-    const state = {route: {name: 'tasks'}, task: {byId}};
+    const state = makeState({route: {name: 'tasks'}, task: tasks});
     const expected = {
       pending: [],
       undone: [parentTask, subTask, subSubTask],
@@ -87,14 +75,14 @@ describe('getPartitionedTasksForRoute', () => {
   });
 
   it('returns root tasks when route is "/tasks/root"', () => {
-    const state = {route: {name: 'rootTasks'}, task: {byId}};
+    const state = makeState({route: {name: 'rootTasks'}, task: tasks});
     const expected = {pending: [], undone: [parentTask]};
 
     expect(getPartitionedTasksForRoute(state)).toEqual(expected);
   });
 
   it('returns leaf tasks when route is "/tasks/leaf"', () => {
-    const state = {route: {name: 'leafTasks'}, task: {byId}};
+    const state = makeState({route: {name: 'leafTasks'}, task: tasks});
     const expected = {pending: [], undone: [subSubTask]};
 
     expect(getPartitionedTasksForRoute(state)).toEqual(expected);
