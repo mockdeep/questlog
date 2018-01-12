c19339d5f3691bd9fe87e5a57af85e0baa6b8cd2
Robert Fletcher <lobatifricha@gmail.com>
2018-01-12 12:14:41 -0800

    DeadCode: remove condition in task/selectors
    This error is not reachable, being that `grab` will throw an error
    first.

--- spec/javascript/task/components/task_row_spec.js
+++ spec/javascript/task/components/task_row_spec.js
@@ -7,10 +7,17 @@ const props = {
   deleteTask: jest.fn(),
   updateTask: jest.fn(),
   keyPrefix: 'testPrefix',
+  task: {},
 };
 
 it('renders a table row', () => {
-  const component = shallow(<TaskRow {...props} task={{}} />);
+  const component = shallow(<TaskRow {...props} />);
 
   expect(component.find('tr')).toHaveClassName('task-list__row');
+});
+
+it('renders the timeframe selector when enabled', () => {
+  const component = shallow(<TaskRow {...props} timeframesEnabled />);
+
+  expect(component.find('select.timeframe-select')).toBePresent();
 });
