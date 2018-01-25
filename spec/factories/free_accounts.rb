c11ce575e2d029efb13f545e794321ed11849000
Robert Fletcher <lobatifricha@gmail.com>
2018-01-24 17:17:15 -0800

    Upgrades: upgrade testing gems
    `FactoryGirl` was renamed to `FactoryBot`.

--- spec/factories/free_accounts.rb
+++ spec/factories/free_accounts.rb
@@ -1,6 +1,4 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
+FactoryBot.define do
   factory :free_account do
     email { Faker::Internet.email }
     password { 'blahblah' }
--- spec/factories/stats.rb
+++ spec/factories/stats.rb
@@ -1,4 +1,4 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :stat do
     user
     sequence(:value) { |n| n }
