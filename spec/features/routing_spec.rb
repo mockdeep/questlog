a49492f8aac5a119e3e0d4fd9a3a50cf95b6e840
Robert Fletcher <lobatifricha@gmail.com>
2018-01-26 13:13:03 -0800

    Specs: add assertions to routing_spec
    Some inconsistencies on CircleCI, so hoping this fixes things.

--- spec/features/routing_spec.rb
+++ spec/features/routing_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe 'front end routing', js: true do
     add_task('do laundry')
     sidebar.click('ALL TASKS')
     expect(current_tasks).to have_task('do laundry')
+    expect(page).to have_no_selector('.task-display')
 
     page.go_back
 
+    expect(page).to have_selector('.task-display')
     expect(page).to have_task('do laundry')
   end
 
