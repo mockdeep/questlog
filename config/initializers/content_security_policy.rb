32426ff689207a093fe112b008785d6317ca2cc1
Robert Fletcher <lobatifricha@gmail.com>
2018-04-20 20:23:31 -0700

    Upgrades: update webpacker installation
    Left in scss support as we may end up switching to that soon.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -2,17 +2,25 @@
 
 # Define an application-wide content security policy
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
+Rails.application.config.content_security_policy do |policy|
+  #   policy.default_src :self, :https
+  #   policy.font_src    :self, :https, :data
+  #   policy.img_src     :self, :https, :data
+  #   policy.object_src  :none
+  #   policy.script_src  :self, :https
+  #   policy.style_src   :self, :https
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+  #   # Specify URI for violation reports
+  #   # policy.report_uri "/csp-violation-report-endpoint"
+  if Rails.env.development?
+    policy.connect_src(
+      :self,
+      :https,
+      'http://localhost:3035',
+      'ws://localhost:3035',
+    )
+  end
+end
 
 # If you are using UJS then enable automatic nonce generation
 # generator = -> request { SecureRandom.base64(16) }
