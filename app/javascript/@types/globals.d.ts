dd434e57313ccafc058d8dbb41da9ee0faebb04c
Robert Fletcher <lobatifricha@gmail.com>
2018-08-18 09:47:09 -0700

    TypeScript: unnest globals and add jquery types

--- app/javascript/@types/globals.d.ts
+++ app/javascript/@types/globals.d.ts
@@ -1,31 +1,28 @@
-declare var $: any;
 declare var debug: any;
 
-export declare global {
-  interface Window {
-    $: any;
-    jQuery: any;
-    debug: any;
-    gon: any;
-    Headers: any;
-    balanceTime: any;
-  }
+interface Window {
+  $: any;
+  jQuery: any;
+  debug: any;
+  gon: any;
+  Headers: any;
+  balanceTime: any;
+}
 
-  interface Function {
-    propTypes: any;
-  }
+interface Function {
+  propTypes: any;
+}
 
-  interface Process {
-    browser: boolean;
-  }
-  interface State {
-    route: any,
-    tag: any,
-    task: any,
-  }
+interface Process {
+  browser: boolean;
+}
+interface State {
+  route: any,
+  tag: any,
+  task: any,
+}
 
-  interface Task {
-    id: number,
-    parentTaskId: number,
-  }
+interface Task {
+  id: number,
+  parentTaskId: number,
 }
