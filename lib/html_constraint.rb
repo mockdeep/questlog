adbe543bb56cda920eac2560beb52bed9c6d5037
Robert Fletcher <lobatifricha@gmail.com>
2018-08-19 20:47:36 -0700

    CI: add */* to HtmlConstraint
    Codeship uses `wget` to test the URL, which sends `Accept: */*` by
    default.  However, it gets a 404 since the constraint only allows for
    `text/html`. This updates it to also accept `*/*`.

--- lib/html_constraint.rb
+++ lib/html_constraint.rb
@@ -1,12 +1,9 @@
 class HtmlConstraint
 
   def matches?(request)
-    accept_format(request).include?('text/html')
-  end
-
-  def accept_format(request)
     # not all request headers have an 'Accept', so we default to 'text/html'
-    request.headers['Accept'] || 'text/html'
+    accept_format = request.headers['Accept'] || 'text/html'
+    accept_format.include?('text/html') || accept_format.include?('*/*')
   end
 
 end
