4d8f1afd2f7d6100280d3c80825b5a3e83530916
Robert Fletcher <lobatifricha@gmail.com>
2018-10-20 14:40:31 -0700

    TypeScript: noImplicitAny in scratch folder

--- app/javascript/@types/state.d.ts
+++ app/javascript/@types/state.d.ts
@@ -2,10 +2,15 @@ type NotificationState = {
   trash: 'goober';
 };
 
+type ScratchState = {
+  [scratchKey: string]: any;
+};
+
 type State = {
   common?: any;
   notification?: NotificationState;
   route?: any;
+  scratch?: ScratchState;
   tag?: any;
   task?: any;
   user?: any;
