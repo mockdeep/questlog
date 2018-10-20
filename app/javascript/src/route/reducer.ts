d8444fe586fffdf7dda984df980cdb213ef7be2e
Robert Fletcher <lobatifricha@gmail.com>
2018-10-20 14:36:19 -0700

    TypeScript: noImplicitAny in route folder

--- app/javascript/src/route/reducer.ts
+++ app/javascript/src/route/reducer.ts
@@ -1,12 +1,16 @@
 import createBasicReducer from 'src/_common/create_basic_reducer';
 import {INIT, SET} from 'src/route/action_creators';
 
+type Payload = {
+  foo: string;
+};
+
 export default createBasicReducer({
   [INIT]() {
     return {params: {}};
   },
 
-  [SET](previousState, payload) {
+  [SET](previousState: State, payload: Payload) {
     return payload;
   },
 });
