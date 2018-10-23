d1cfcd4419fa1e4ebf94c8c2ae27b580dabbfa3e
Robert Fletcher <mockdeep@users.noreply.github.com>
2018-10-23 11:02:34 -0700

    TypeScript: noImplicitAny in timeframe folder (#69)

--- app/javascript/@types/timeframe.d.ts
+++ app/javascript/@types/timeframe.d.ts
@@ -7,6 +7,34 @@ type TimeframeName = 'inbox'
   | 'lustrum'
   | 'decade';
 
+type Timeframe = {
+  currentTasks: Task[];
+  pendingTasks: Task[];
+  name: TimeframeName;
+  medianProductivity?: number;
+  minuteMax?: number;
+};
+
 type TimeframeSpace = {
   [timeframeName: string]: number;
+};
+
+type TimeframeData = {
+  timeframes: Timeframe[];
+  meta: {medianProductivity: number};
+};
+
+type TimeframeStoreType = {
+  listeners: Callback[];
+  models: Timeframe[];
+  getAll(): Promise<TimeframeData>;
+  subscribe(listener: Callback): Callback;
+  loaded: boolean;
+  name: 'timeframe';
+  url: '/timeframes';
+  unsubscribe(listener: Callback): void;
+  notifyListeners(): void;
+  unload(): void;
+  updateModels(data: {tasks: Task[]}): void;
+  getState(): TimeframeData;
 };
--- spec/javascript/timeframe/containers/list_view_spec.tsx
+++ spec/javascript/timeframe/containers/list_view_spec.tsx
@@ -4,9 +4,9 @@ import {shallow} from 'enzyme';
 import createAppStore from 'src/create_app_store';
 import TimeframeListViewContainer from 'src/timeframe/containers/list_view';
 
+const props = {store: createAppStore()};
 it('wraps the TimeframeListView component', () => {
-  const store = createAppStore();
-  const container = shallow(<TimeframeListViewContainer store={store} />);
+  const container = shallow(<TimeframeListViewContainer {...props} />);
 
   expect(container.find('TimeframeListView')).toHaveLength(1);
 });
