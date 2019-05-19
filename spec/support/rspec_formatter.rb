8519ac724602918842b8cca27ad3ef027b350288
Robert Fletcher <mockdeep@users.noreply.github.com>
2019-05-19 09:25:14 -0700

    CI: reintroduce CircleCI config (#172)
    * Cleaned up some existing stuff that was leftover from the previous
    integration.
    * Removed the `node` engine because the default on the
      CircleCI build machine is `v8.10.0`. I don't think it should matter
      all that much, as long as the version is recent, so we'll leave it off
      for now.
    * ignored the `vendor/` directory from various linters, as the gems get
      vendored there.

--- spec/support/rspec_formatter.rb
+++
@@ -1,9 +0,0 @@
-if ENV['CIRCLECI']
-  require 'rspec_junit_formatter'
-
-  RSpec.configure do |config|
-    output_path = File.join(ENV['CIRCLE_TEST_REPORTS'], 'rspec', 'junit.xml')
-    # second arg to `#add_formatter` is private rspec api, so watch for breakage
-    config.add_formatter(RspecJunitFormatter, output_path)
-  end
-end
