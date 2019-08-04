8b2e66ba837db3db59228e69e8b8a4d1bfeffe91
Robert Fletcher <lobatifricha@gmail.com>
2019-08-03 19:34:44 -0700

    remove Elm
    After more consideration, I don't think Elm is the right way to go.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,7 @@
 const {environment} = require('@rails/webpacker');
 
-const elm = require('./loaders/elm');
 const typescript = require('./loaders/typescript');
 
-environment.loaders.append('elm', elm);
 environment.loaders.append('typescript', typescript);
 
 module.exports = environment;
