14763554631d864514ee56a5458ee50ed0f512bd
Robert Fletcher <mockdeep@users.noreply.github.com>
2020-03-17 15:00:48 -0700

    Split up test factories (#889)
    I want to add more to these factories, so it'll be easier to have them
    separate.

---
+++ spec/javascript/_test_helpers/factories/timeframe.ts
@@ -0,0 +1,12 @@
+function makeTimeframe(attrs: Partial<Timeframe>): Timeframe {
+  return {
+    name: 'inbox',
+    medianProductivity: 30,
+    minuteMax: Infinity,
+    currentTasks: [],
+    pendingTasks: [],
+    ...attrs,
+  };
+}
+
+export {makeTimeframe};
