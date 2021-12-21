9f1382a65a787893e6fb8ffb1dd87bff0ab4874b
Robert Fletcher <mockdeep@users.noreply.github.com>
2021-12-21 09:55:25 -0800

    upgrade to all 6.1 defaults (#1671)

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,7 @@ module Questlog
   class Application < Rails::Application
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-
-    config.autoloader = :zeitwerk
+    config.load_defaults 6.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
