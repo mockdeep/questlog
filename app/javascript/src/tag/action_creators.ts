904e83c57c6688e88495ac809bc30db7796bee35
Robert Fletcher <mockdeep@users.noreply.github.com>
2022-02-27 12:18:41 -0800

    NoReact: update tags synchronously (#1728)
    Rather than updating tags via ajax, this makes the form to update tag
    rules a synchronous form submit.

--- app/javascript/src/tag/action_creators.ts
+++ app/javascript/src/tag/action_creators.ts
@@ -1,29 +1,12 @@
-import {Action, Dispatch} from 'redux';
+import {Action} from 'redux';
 import {ThunkAction} from 'redux-thunk';
-
-import {ajaxPut} from 'src/_helpers/ajax';
-
-const BASE_PATH = '/api/v1/tags';
 
 const INIT = 'tag/INIT';
 const SET = 'tag/SET';
-const UPDATE = 'tag/UPDATE';
 const UPSERT = 'tag/UPSERT';
 
 function setTags(payload: Tag[]) {
   return {type: SET, payload};
-}
-
-function updateTagPlain(id: number, payload: AjaxTag) {
-  return {type: UPDATE, payload: {id, ...payload}};
-}
-
-function updateTag(id: number, payload: AjaxTag) {
-  return async function updateTagThunk(dispatch: Dispatch) {
-    await ajaxPut(`${BASE_PATH}/${id}`, {tag: payload});
-
-    dispatch(updateTagPlain(id, payload));
-  };
 }
 
 function upsertTagPlain(payload: AjaxTag) {
@@ -36,5 +19,5 @@ function upsertTags(tags: AjaxTag[]): ThunkAction<void, State, null, Action> {
   };
 }
 
-export {INIT, SET, UPDATE, UPSERT};
-export {setTags, updateTag, upsertTagPlain, upsertTags};
+export {INIT, SET, UPSERT};
+export {setTags, upsertTagPlain, upsertTags};
--- app/javascript/src/tag/containers/edit_view.ts
+++ app/javascript/src/tag/containers/edit_view.ts
@@ -1,13 +1,9 @@
 import {connect} from 'react-redux';
 import TagEditView from 'src/tag/components/edit_view';
 import {getSelectedTag} from 'src/tag/selectors';
-import {setRoute} from 'src/route/action_creators';
-import {updateTag} from 'src/tag/action_creators';
 
 function mapStateToProps(state: State) {
   return {tag: getSelectedTag(state)};
 }
 
-const actionCreators = {setRoute, updateTag};
-
-export default connect(mapStateToProps, actionCreators)(TagEditView);
+export default connect(mapStateToProps)(TagEditView);
