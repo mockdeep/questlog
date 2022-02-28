77c35ef644a493d6346d51194754766fcacadd3e
Robert Fletcher <mockdeep@users.noreply.github.com>
2022-02-27 21:24:16 -0800

    NoReact: move bulk task submission to synchronous (#1734)

--- spec/javascript/task/components/bulk_add_view_spec.tsx
+++ spec/javascript/task/components/bulk_add_view_spec.tsx
@@ -4,22 +4,11 @@ import React from 'react';
 import {mount} from 'enzyme';
 
 import TaskBulkAddView from 'src/task/components/bulk_add_view';
-import BulkTaskStore from 'src/task/bulk_store';
 
-const setRoute = jest.fn();
-const props = {setRoute, taskTitles: 'foobutts', updateTaskMeta: jest.fn()};
+const props = {taskTitles: 'foobutts', updateTaskMeta: jest.fn()};
 
-it('redirects to TaskList on successful save', () => {
+it('renders a bulk form', () => {
   const wrapper = mount(<TaskBulkAddView {...props} />);
-  const fakeThen = jest.fn();
 
-  (BulkTaskStore.create as jest.Mock).mockReturnValueOnce({then: fakeThen});
-
-  expect(BulkTaskStore.create).not.toHaveBeenCalled();
-  wrapper.find('form').simulate('submit');
-  expect(BulkTaskStore.create).toHaveBeenCalledWith({titles: 'foobutts'});
-
-  expect(setRoute).not.toHaveBeenCalled();
-  fakeThen.mock.calls[0][0]();
-  expect(setRoute).toHaveBeenCalledWith({name: 'tasks'});
+  expect(wrapper.find('form')).toHaveLength(1);
 });
