39bafe7e83f3876fe5bcc0cc0c051fe52a40b438
Robert Fletcher <lobatifricha@gmail.com>
2022-03-02 21:10:23 -0800

    NoReact: render anchors directly for task links

--- spec/javascript/task/components/link_spec.tsx
+++ spec/javascript/task/components/link_spec.tsx
@@ -5,16 +5,13 @@ import TaskLink from 'src/task/components/link';
 
 import {makeTask} from '_test_helpers/factories';
 
-const Link = 'Connect(Link)';
-
 it('renders a link to the task', () => {
   const task = makeTask({title: 'my title'});
   const component = shallow(<TaskLink task={task} />);
 
-  const link = component.find(Link);
+  const link = component.find('a');
 
   expect(link).toHaveProp('children', task.title);
-  expect(link).toHaveProp('to', 'showTask');
+  expect(link).toHaveProp('href', `/tasks/${task.id}`);
   expect(link).toHaveProp('className', 'task-link');
-  expect(link).toHaveProp('params', {taskId: task.id});
 });
