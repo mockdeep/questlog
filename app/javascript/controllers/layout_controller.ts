951a3d7b848af61627c268c82eede79caf0980e1
Robert Fletcher <lobatifricha@gmail.com>
2022-03-03 21:23:24 -0800

    NoReact: introduce server rendered sidebar

---
+++ app/javascript/controllers/layout_controller.ts
@@ -0,0 +1,30 @@
+import {Controller} from '@hotwired/stimulus';
+
+class LayoutController extends Controller {
+  static targets = ['expandedSidebar', 'collapsedSidebar'];
+
+  expandedSidebarTarget!: HTMLElement;
+
+  collapsedSidebarTarget!: HTMLElement;
+
+  updateScreenSize() {
+    const visible = window.innerWidth > 600;
+    this.toggleSidebarVisibility({visible});
+  }
+
+  toggleSidebarVisibility({visible}: {visible: boolean}) {
+    this.element.classList.toggle('sidebar-open', visible);
+    this.expandedSidebarTarget.classList.toggle('hide-me', !visible);
+    this.collapsedSidebarTarget.classList.toggle('hide-me', visible);
+  }
+
+  hideSidebar() {
+    this.toggleSidebarVisibility({visible: false});
+  }
+
+  showSidebar() {
+    this.toggleSidebarVisibility({visible: true});
+  }
+}
+
+export default LayoutController;
