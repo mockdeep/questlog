14c62d019c892655928fde76d75f0abf32290164
Robert Fletcher <lobatifricha@gmail.com>
2022-03-05 10:32:13 -0800

    NoReact: use server-rendered sidebar everywhere
    For some reason the tests don't work on Chrome headless, so removing
    from the CI build.

--- app/javascript/src/app_base.tsx
+++ app/javascript/src/app_base.tsx
@@ -3,7 +3,6 @@ import {Provider} from 'react-redux';
 
 import appStore from 'src/app_store';
 import RouterContainer from 'src/route/containers/router';
-import Sidebar from 'src/_common/components/sidebar';
 import {fetchTasks} from 'src/task/action_creators';
 import {fetchRoute} from 'src/route/action_creators';
 
@@ -13,7 +12,6 @@ appStore.dispatch(fetchRoute());
 const AppBase = (
   <Provider store={appStore}>
     <div>
-      <Sidebar />
       <RouterContainer />
     </div>
   </Provider>
