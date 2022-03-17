40998c8ffb7cba06bbddac95856f6a99dc363938
Robert Fletcher <mockdeep@users.noreply.github.com>
2022-03-16 21:24:44 -0700

    Specs: only freeze Date (#1766)
    By default FakeTimers fakes a whole bunch of browser APIs, including
    `requestAnimationFrame`, which is used heavily by Turbo. When it's
    faked, it doesn't call the animation callback until you manually trigger
    it via the fake clock. We only really need `Date` currently, as that's
    what is used to calculate timeframes.

--- app/javascript/packs/freeze_time.ts
+++ app/javascript/packs/freeze_time.ts
@@ -3,7 +3,7 @@ import FakeTimers from '@sinonjs/fake-timers';
 const element = document.querySelector('.time-freeze');
 if (element instanceof HTMLElement) {
   const now = Number(element.dataset.timestamp);
-  FakeTimers.install({now});
+  FakeTimers.install({now, toFake: ['Date']});
 } else {
   throw new Error('element is not HTMLElement');
 }
