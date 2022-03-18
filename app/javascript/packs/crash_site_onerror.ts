729d42aee7e44c6f5822f62821814a3f1c520091
Robert Fletcher <mockdeep@users.noreply.github.com>
2022-03-17 23:18:58 -0700

    Linters: add script to display error on JS errors (#1773)

---
+++ app/javascript/packs/crash_site_onerror.ts
@@ -0,0 +1,29 @@
+// eslint-disable-next-line max-params
+window.onerror = (...args) => {
+  if (document.readyState === 'complete') {
+    displayError(...args);
+  } else {
+    document.onreadystatechange = () => {
+      if (document.readyState !== 'complete') { return; }
+      displayError(...args);
+    };
+  }
+};
+
+// eslint-disable-next-line max-params
+function displayError(
+  message: string | Event,
+  url: string | undefined,
+  lineNumber: number | undefined,
+  column: number | undefined,
+  error: Error | undefined,
+) {
+  document.body.innerHTML = `
+    <h1>javascript error while testing:</h1>
+    message: ${message}<br />
+    url: ${url}<br />
+    line number: ${lineNumber}<br />
+    column: ${column}<br />
+    stack: ${error && error.stack && error.stack.replaceAll('\n', '<br />')}
+  `;
+}
