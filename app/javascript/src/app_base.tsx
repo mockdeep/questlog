556154c2524ba8257f331c76279c69c5660b1cd2
Robert Fletcher <mockdeep@users.noreply.github.com>
2022-03-17 22:23:06 -0700

    NoReact: make AppBase a React component (#1772)
    Static rendering it causes it to be loaded on every page, even those
    without React.

--- app/javascript/src/app_base.tsx
+++ app/javascript/src/app_base.tsx
@@ -3,18 +3,15 @@ import {Provider} from 'react-redux';
 
 import appStore from 'src/app_store';
 import RouterContainer from 'src/route/containers/router';
-import {fetchTasks} from 'src/task/action_creators';
-import {fetchRoute} from 'src/route/action_creators';
 
-appStore.dispatch(fetchTasks());
-appStore.dispatch(fetchRoute());
-
-const AppBase = (
-  <Provider store={appStore}>
-    <div>
-      <RouterContainer />
-    </div>
-  </Provider>
-);
+function AppBase() {
+  return (
+    <Provider store={appStore}>
+      <div>
+        <RouterContainer />
+      </div>
+    </Provider>
+  );
+}
 
 export default AppBase;
