52c774725cffd3cf233cbfba78af3ec636f2b5de
Robert Fletcher <mockdeep@users.noreply.github.com>
2022-03-19 15:12:14 -0700

    NoReact: remove Router component (#1779)
    Handle it directly within `ReactController`.

--- app/javascript/src/route/containers/router.ts
+++
@@ -1,9 +0,0 @@
-import {connect} from 'react-redux';
-
-import Router from 'src/route/components/router';
-
-function mapStateToProps(state: State) {
-  return {route: state.route};
-}
-
-export default connect(mapStateToProps)(Router);
