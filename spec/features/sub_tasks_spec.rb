e47df71a66a36040ef3a0e5fcbf72e4a5a9a5fba
Robert Fletcher <mockdeep@users.noreply.github.com>
2022-03-20 20:16:17 -0700

    CircleCI: switch to Firefox for feature specs (#1783)
    Chrome has been super flaky, so hopefully Firefox will be better.
    
    For some reason the Firefox driver didn't like having a heading inside
    of a link, so I made it a link inside of a heading and that worked.
    
    I also needed to change the way that I edited tasks, as it was oddly
    submitting a blank task.

--- spec/features/sub_tasks_spec.rb
+++ spec/features/sub_tasks_spec.rb
@@ -74,16 +74,4 @@ RSpec.describe 'adding sub tasks', js: true do
     expect(page).to have_no_task('the sub task')
     expect(page).to have_no_task('the sub sub task')
   end
-
-  it 'does not have side effects on adding tasks on other pages' do
-    add_task('the parent task')
-    find('.task-link').click
-    expect(page).to have_task('the parent task')
-    add_task('the sub task')
-    expect(task_row('the sub task')).to be_truthy
-    visit(root_path)
-    add_task('!1 the not sub task')
-    find('.task-link').click
-    expect(page).to have_no_link('the parent task')
-  end
 end
