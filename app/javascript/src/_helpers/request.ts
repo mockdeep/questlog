80fba51ca82259e2c88d79b553a94e0fb2a35b4a
Robert Fletcher <lobatifricha@gmail.com>
2023-12-28 22:18:10 -0600

    replace reqwest with fetch (#2107)
    followups will remove remaining references.

--- app/javascript/src/_helpers/request.ts
+++ app/javascript/src/_helpers/request.ts
@@ -1,6 +1,5 @@
 import type {BaseRequestOptions} from 'reqwest';
 import authenticityToken from 'src/_helpers/authenticity_token';
-import reqwest from 'reqwest';
 
 type RequestOptions = {
   data?: unknown;
@@ -8,25 +7,22 @@ type RequestOptions = {
   success: (data: any) => void;
 };
 
-function logError(error: DOMException): void {
-  // eslint-disable-next-line no-console
-  console.log('error: ', error);
-}
-
 function defaultRequestOptions(): BaseRequestOptions {
   return {
-    headers: {
+    headers: new Headers({
       Accept: 'application/json',
       'Content-Type': 'application/json',
       'X-CSRF-Token': authenticityToken(),
-    },
-    error: logError,
+    }),
+    credentials: 'same-origin',
   };
 }
 
 export default function request(url: string, options: RequestOptions): void {
-  const {data, success} = options;
+  const {success} = options;
+  const body = JSON.stringify(options.data);
   const method = options.method ?? 'PUT';
 
-  void reqwest({...defaultRequestOptions(), method, success, data, url});
+  void fetch(url, {...defaultRequestOptions(), body, method})
+    .then(response => { response.json().then(success); });
 }
