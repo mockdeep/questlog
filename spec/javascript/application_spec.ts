228197512bf5517d3572c9bdd58edab2406e97e5
Robert Fletcher <lobatifricha@gmail.com>
2026-02-19 09:53:29 -0800

    flatten packs directory (#2622)

--- spec/javascript/application_spec.ts
+++ spec/javascript/application_spec.ts
@@ -1,10 +1,8 @@
 vi.mock("react-dom");
 vi.mock("controllers/index", () => { return {}; });
 vi.mock("@hotwired/turbo-rails");
+import "javascript/application";
 
-import "src/application";
-import {debug} from "helpers";
-
-it("sets debug as a global", () => {
-  expect(window.debug).toBe(debug);
+it("sets up the application", () => {
+  expect(window.jQuery).not.toBeUndefined();
 });
