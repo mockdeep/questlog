a221265a00be0d67d108b121e591f1b932101483
Robert Fletcher <lobatifricha@gmail.com>
2026-02-18 16:51:00 -0800

    flatten packs directory

--- spec/javascript/packs/application_spec.ts
+++
@@ -1,8 +0,0 @@
-vi.mock("react-dom");
-vi.mock("controllers/index", () => { return {}; });
-vi.mock("@hotwired/turbo-rails");
-import "packs/application";
-
-it("sets up the application", () => {
-  expect(window.jQuery).not.toBeUndefined();
-});
--- vitest.config.ts
+++ vitest.config.ts
@@ -16,7 +16,6 @@ export default defineConfig({
       {find: /^helpers\//u, replacement: appPath("helpers")},
       {find: /^helpers$/u, replacement: `${path.resolve(root, "app/javascript/helpers/index.ts")}`},
       {find: /^javascript\//u, replacement: appPath("")},
-      {find: /^packs\//u, replacement: appPath("packs")},
       {find: /^spec\//u, replacement: `${path.resolve(root, "spec")}/`},
       {find: /^src\//u, replacement: appPath("")},
     ],
