4479c3c47f1ff7c63459dfa4c99702e9b7006db7
Robert Fletcher <lobatifricha@gmail.com>
2026-08-29 09:28:47 -0700

    Errors: block json requests by default
    **What**
    
    Update routes to require html format.
    
    **Why**
    
    We got [an error][er] from some penetration bot in Australia. This will
    block those sorts of requests with a 404 instead of reporting an
    application error.
    
    [er]: https://app.honeybadger.io/projects/33567/faults/133920510

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root "pages#index"
+  scope defaults: { format: :html }, format: false do
+    root "pages#index"
 
-  resource :session, only: [:new, :create, :destroy]
+    resource :session, only: [:new, :create, :destroy]
 
-  resource :bulk_task, only: [:new, :create]
-  resources :free_accounts, only: [:new, :create]
-  resources :charges, only: [:new, :create]
-  resources :root_tasks, only: [:index]
-  resources :leaf_tasks, only: [:index]
-  resources :tree_tasks, only: [:index]
-  resources :tags, only: [:index, :show, :edit, :update]
-  resources :tasks, only: [:index, :create, :show, :update, :destroy]
-  resources :timeframes, only: [:index]
-  resources :alpha, only: [:index]
-  resources :help, only: [:index]
-  get "/what", to: "pages#what"
-  get "/privacy", to: "pages#privacy"
+    resource :bulk_task, only: [:new, :create]
+    resources :free_accounts, only: [:new, :create]
+    resources :charges, only: [:new, :create]
+    resources :root_tasks, only: [:index]
+    resources :leaf_tasks, only: [:index]
+    resources :tree_tasks, only: [:index]
+    resources :tags, only: [:index, :show, :edit, :update]
+    resources :tasks, only: [:index, :create, :show, :update, :destroy]
+    resources :timeframes, only: [:index]
+    resources :alpha, only: [:index]
+    resources :help, only: [:index]
+    get "/what", to: "pages#what"
+    get "/privacy", to: "pages#privacy"
+  end
 end
